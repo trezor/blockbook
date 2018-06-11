914a8140a9f0a473b0a642ca2c22d04dd733ab0e
Martin Boehm <martin.boehm@1mbsoftware.net>
2018-06-11 18:02:25 +0200

    Add litecoin blockbook implementation

---
+++ bchain/coins/litecoin/litecoinrpc.go
@@ -0,0 +1,56 @@
+package litecoin
+
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"encoding/json"
+
+	"github.com/golang/glog"
+)
+
+// LitecoinRPC is an interface to JSON-RPC bitcoind service.
+type LitecoinRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewLitecoinRPC returns new LitecoinRPC instance.
+func NewLitecoinRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &LitecoinRPC{
+		b.(*btc.BitcoinRPC),
+	}
+	s.RPCMarshaler = btc.JSONMarshalerV2{}
+
+	return s, nil
+}
+
+// Initialize initializes LitecoinRPC instance.
+func (b *LitecoinRPC) Initialize() error {
+	chainName, err := b.GetChainInfoAndInitializeMempool(b)
+	if err != nil {
+		return err
+	}
+
+	glog.Info("Chain name ", chainName)
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewLitecoinParser(params, b.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		b.Testnet = false
+		b.Network = "livenet"
+	} else {
+		b.Testnet = true
+		b.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
