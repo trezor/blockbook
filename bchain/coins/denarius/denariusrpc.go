e09d2e2915f784d547bb90630b9bad7c0140f408
carsenk <carsenk@gmail.com>
2018-07-02 06:39:05 -0600

    Add getblock for DNR

--- bchain/coins/denarius/denariusrpc.go
+++ bchain/coins/denarius/denariusrpc.go
@@ -84,7 +84,70 @@ func (b *DenariusRPC) Initialize() error {
 	return nil
 }
 
+// GetBlock returns block with given hash.
+func (z *DenariusRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
+	var err error
+	if hash == "" && height > 0 {
+		hash, err = z.GetBlockHash(height)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	glog.V(1).Info("rpc: getblock (verbosity=1) ", hash)
+
+	res := btc.ResGetBlockThin{}
+	req := btc.CmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = 1
+	err = z.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+	if res.Error != nil {
+		return nil, errors.Annotatef(res.Error, "hash %v", hash)
+	}
+
+	txs := make([]bchain.Tx, 0, len(res.Result.Txids))
+	for _, txid := range res.Result.Txids {
+		tx, err := z.GetTransaction(txid)
+		if err != nil {
+			if isInvalidTx(err) {
+				glog.Errorf("rpc: getblock: skipping transanction in block %s due error: %s", hash, err)
+				continue
+			}
+			return nil, err
+		}
+		txs = append(txs, *tx)
+	}
+	block := &bchain.Block{
+		BlockHeader: res.Result.BlockHeader,
+		Txs:         txs,
+	}
+	return block, nil
+}
+
+func isInvalidTx(err error) bool {
+	switch e1 := err.(type) {
+	case *errors.Err:
+		switch e2 := e1.Cause().(type) {
+		case *bchain.RPCError:
+			if e2.Code == -5 { // "No information available about transaction"
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 // EstimateFee returns fee estimation.
 func (b *DenariusRPC) EstimateFee(blocks int) (float64, error) {
 	return b.EstimateSmartFee(blocks, true)
+}
+
+func isErrBlockNotFound(err *bchain.RPCError) bool {
+	return err.Message == "Block not found" ||
+		err.Message == "Block height out of range"
 }
