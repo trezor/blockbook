17280f7ee6dff43e5e1901c7905ed8aaa415a5f2
Samad Sajanlal <samad.sajanlal@gmail.com>
2018-10-08 02:37:03 -0500

    add GameCredits

---
+++ bchain/coins/gamecredits/gamecreditsparser.go
@@ -0,0 +1,66 @@
+package gamecredits
+
+import (
+	"blockbook/bchain/coins/btc"
+
+	"github.com/btcsuite/btcd/wire"
+	"github.com/jakm/btcutil/chaincfg"
+)
+
+const (
+	MainnetMagic wire.BitcoinNet = 0xdbb6c0fb
+	TestnetMagic wire.BitcoinNet = 0x0709110b
+	RegtestMagic wire.BitcoinNet = 0xdab5bffa
+)
+
+var (
+	MainNetParams chaincfg.Params
+	TestNetParams chaincfg.Params
+)
+
+func init() {
+	MainNetParams = chaincfg.MainNetParams
+	MainNetParams.Net = MainnetMagic
+	MainNetParams.PubKeyHashAddrID = []byte{48}
+	MainNetParams.ScriptHashAddrID = []byte{50}
+	MainNetParams.Bech32HRPSegwit = "game"
+
+	TestNetParams = chaincfg.TestNet3Params
+	TestNetParams.Net = TestnetMagic
+	TestNetParams.PubKeyHashAddrID = []byte{111}
+	TestNetParams.ScriptHashAddrID = []byte{58}
+	TestNetParams.Bech32HRPSegwit = "tgame"
+}
+
+// GameCreditsParser handle
+type GameCreditsParser struct {
+	*btc.BitcoinParser
+}
+
+// NewGameCreditsParser returns new GameCreditsParser instance
+func NewGameCreditsParser(params *chaincfg.Params, c *btc.Configuration) *GameCreditsParser {
+	return &GameCreditsParser{BitcoinParser: btc.NewBitcoinParser(params, c)}
+}
+
+// GetChainParams contains network parameters for the main GameCredits network,
+// and the test GameCredits network
+func GetChainParams(chain string) *chaincfg.Params {
+	if !chaincfg.IsRegistered(&chaincfg.MainNetParams) {
+		chaincfg.RegisterBitcoinParams()
+	}
+	if !chaincfg.IsRegistered(&MainNetParams) {
+		err := chaincfg.Register(&MainNetParams)
+		if err == nil {
+			err = chaincfg.Register(&TestNetParams)
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+	switch chain {
+	case "test":
+		return &TestNetParams
+	default:
+		return &MainNetParams
+	}
+}
