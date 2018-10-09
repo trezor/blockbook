b888270f90beba9f1a349575431e17601a478ee4
Martin Boehm <martin.boehm@1mbsoftware.net>
2018-10-09 12:13:38 +0200

    Add comment explaining registered parameters in litecoin parser

--- bchain/coins/litecoin/litecoinparser.go
+++ bchain/coins/litecoin/litecoinparser.go
@@ -45,6 +45,9 @@ func NewLitecoinParser(params *chaincfg.Params, c *btc.Configuration) *LitecoinP
 // GetChainParams contains network parameters for the main Litecoin network,
 // and the test Litecoin network
 func GetChainParams(chain string) *chaincfg.Params {
+	// register bitcoin parameters in addition to litecoin parameters
+	// litecoin has dual standard of addresses and we want to be able to
+	// parse both standards
 	if !chaincfg.IsRegistered(&chaincfg.MainNetParams) {
 		chaincfg.RegisterBitcoinParams()
 	}
