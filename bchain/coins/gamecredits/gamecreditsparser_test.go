e363706f66ab840159131eaf2725af43f513919d
Samad Sajanlal <samad.sajanlal@gmail.com>
2018-10-18 14:09:05 -0500

    update parser test

--- bchain/coins/gamecredits/gamecreditsparser_test.go
+++ bchain/coins/gamecredits/gamecreditsparser_test.go
@@ -111,22 +111,14 @@ func init() {
 		Hex:       "010000000123c41ad26dd5782635638effbc9e31c9b4a3b757591a52c83d2770ad82b33e93000000006b483045022100a20302bde6d2fb194bb9c0a8d7beb52ed0b5b72b912da75364efe169d5b74c67022065632d4032673a6093f513b93e380323487ad2708003e161a12e7b7362bf9f4a01210325c1b08d90a016cb73f4e8d37614cac7da00cb78121f21b7b6e0a7d4a03fbae4fdffffff0100f4aa01000000001976a914ca093a938a0e19e86b36859d9423a475d45eb3a288acc54f2100",
 		Blocktime: 1539653891,
 		Txid:      "983da8317fff45afb17290d4dd8da6ec1cd8ffbbfa98e53a0754e9b60f8cc0f9",
-		LockTime:  0,
-		Version:   536870912,
+		LockTime:  2183109,
+		Version:   1,
 		Vin: []bchain.Vin{
 			{
 				ScriptSig: bchain.ScriptSig{
-					Hex: "",
+					Hex: "483045022100a20302bde6d2fb194bb9c0a8d7beb52ed0b5b72b912da75364efe169d5b74c67022065632d4032673a6093f513b93e380323487ad2708003e161a12e7b7362bf9f4a01210325c1b08d90a016cb73f4e8d37614cac7da00cb78121f21b7b6e0a7d4a03fbae4",
 				},
-				Txid:     "55fe0fdf4fb40ae3614005af836a2a8669e632dfc685a1cb9a9d2caf4c0e109c",
-				Vout:     1,
-				Sequence: 0,
-			},
-			{
-				ScriptSig: bchain.ScriptSig{
-					Hex: "",
-				},
-				Txid:     "983da8317fff45afb17290d4dd8da6ec1cd8ffbbfa98e53a0754e9b60f8cc0f9",
+				Txid:     "933eb382ad70273dc8521a5957b7a3b4c9319ebcff8e63352678d56dd21ac423",
 				Vout:     0,
 				Sequence: 4294967293,
 			},
@@ -141,17 +133,7 @@ func init() {
 						"GcGBy77CCfZJJhGLALohdahf9eAc7jo7Yk",
 					},
 				},
-			},
-			{
-				ValueSat: *big.NewInt(84896365),
-				N:        1,
-				ScriptPubKey: bchain.ScriptPubKey{
-					Hex: "a9146edfea548a7d6c25aa28e37bf2ea382891882fa687",
-					Addresses: []string{
-						"S84eckDshWupTwErdLKkyDauNwtWfa9rPL",
-					},
-				},
-			},
+			}
 		},
 	}
 }
