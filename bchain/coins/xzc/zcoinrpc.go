59e0032724d7dbd57695fb8853aef11ff43b8ea3
Putta Khunchalee <putta@zcoin.io>
2019-01-10 17:59:56 +0700

    Finish BlockChain implementation for XZC

--- bchain/coins/xzc/zcoinrpc.go
+++ bchain/coins/xzc/zcoinrpc.go
@@ -3,7 +3,11 @@ package xzc
 import (
 	"blockbook/bchain"
 	"blockbook/bchain/coins/btc"
+	"encoding/hex"
 	"encoding/json"
+
+	"github.com/golang/glog"
+	"github.com/juju/errors"
 )
 
 type ZcoinRPC struct {
@@ -11,14 +15,148 @@ type ZcoinRPC struct {
 }
 
 func NewZcoinRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
-	base, err := btc.NewBitcoinRPC(config, pushHandler)
+	bc, err := btc.NewBitcoinRPC(config, pushHandler)
 	if err != nil {
 		return nil, err
 	}
 
-	zcoin := &ZcoinRPC{
-		BitcoinRPC: base.(*btc.BitcoinRPC),
+	zc := &ZcoinRPC{
+		BitcoinRPC: bc.(*btc.BitcoinRPC),
 	}
 
-	return zcoin, nil
+	zc.ChainConfig.Parse = true
+	zc.ParseBlocks = true
+
+	return zc, nil
+}
+
+func (zc *ZcoinRPC) Initialize() error {
+	chainName, err := zc.GetChainInfoAndInitializeMempool(zc)
+	if err != nil {
+		return err
+	}
+
+	params := GetChainParams(chainName)
+
+	// always create parser
+	zc.Parser = NewZcoinParser(params, zc.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		zc.Testnet = false
+		zc.Network = "livenet"
+	} else {
+		zc.Testnet = true
+		zc.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
+
+func (zc *ZcoinRPC) GetBlockInfo(hash string) (*bchain.BlockInfo, error) {
+	glog.V(1).Info("rpc: getblock (verbosity=true) ", hash)
+
+	res := btc.ResGetBlockInfo{}
+	req := cmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = true
+	err := zc.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+	if res.Error != nil {
+		if isErrBlockNotFound(res.Error) {
+			return nil, bchain.ErrBlockNotFound
+		}
+		return nil, errors.Annotatef(res.Error, "hash %v", hash)
+	}
+	return &res.Result, nil
+}
+
+func (zc *ZcoinRPC) GetBlockRaw(hash string) ([]byte, error) {
+	glog.V(1).Info("rpc: getblock (verbosity=false) ", hash)
+
+	res := btc.ResGetBlockRaw{}
+	req := cmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = false
+	err := zc.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+	if res.Error != nil {
+		if isErrBlockNotFound(res.Error) {
+			return nil, bchain.ErrBlockNotFound
+		}
+		return nil, errors.Annotatef(res.Error, "hash %v", hash)
+	}
+	return hex.DecodeString(res.Result)
+}
+
+func (zc *ZcoinRPC) GetTransactionForMempool(txid string) (*bchain.Tx, error) {
+	glog.V(1).Info("rpc: getrawtransaction nonverbose ", txid)
+
+	res := btc.ResGetRawTransactionNonverbose{}
+	req := cmdGetRawTransaction{Method: "getrawtransaction"}
+	req.Params.Txid = txid
+	req.Params.Verbose = 0
+	err := zc.Call(&req, &res)
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	if res.Error != nil {
+		return nil, errors.Annotatef(res.Error, "txid %v", txid)
+	}
+	data, err := hex.DecodeString(res.Result)
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	tx, err := zc.Parser.ParseTx(data)
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	return tx, nil
+}
+
+func (zc *ZcoinRPC) GetTransactionSpecific(txid string) (json.RawMessage, error) {
+	glog.V(1).Info("rpc: getrawtransaction ", txid)
+
+	res := btc.ResGetRawTransaction{}
+	req := cmdGetRawTransaction{Method: "getrawtransaction"}
+	req.Params.Txid = txid
+	req.Params.Verbose = 1
+	err := zc.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	if res.Error != nil {
+		return nil, errors.Annotatef(res.Error, "txid %v", txid)
+	}
+	return res.Result, nil
+}
+
+func isErrBlockNotFound(err *bchain.RPCError) bool {
+	return err.Message == "Block not found" ||
+		err.Message == "Block height out of range"
+}
+
+type cmdGetBlock struct {
+	Method string `json:"method"`
+	Params struct {
+		BlockHash string `json:"blockhash"`
+		Verbosity bool   `json:"verbosity"`
+	} `json:"params"`
+}
+
+type cmdGetRawTransaction struct {
+	Method string `json:"method"`
+	Params struct {
+		Txid    string `json:"txid"`
+		Verbose int    `json:"verbose"`
+	} `json:"params"`
 }
