b848f17e7a35fac10cfdbe3a6edb8a0fbb67f4ef
thebevrishot <panu.suk@satang.com>
2019-01-11 12:55:46 +0700

    Add unittest for parser && fix wrong network params

--- bchain/coins/xzc/zcoinparser.go
+++ bchain/coins/xzc/zcoinparser.go
@@ -3,12 +3,16 @@ package xzc
 import (
 	"blockbook/bchain"
 	"blockbook/bchain/coins/btc"
+	"encoding/hex"
+	"fmt"
 
 	"github.com/btcsuite/btcd/wire"
 	"github.com/jakm/btcutil/chaincfg"
 )
 
 const (
+	OpZeroCoinMint = 0xc1
+
 	MainnetMagic wire.BitcoinNet = 0xe3d9fef1
 	TestnetMagic wire.BitcoinNet = 0xcffcbeea
 	RegtestMagic wire.BitcoinNet = 0xfabfb5da
@@ -26,16 +30,16 @@ func init() {
 
 	// Address encoding magics
 	MainNetParams.AddressMagicLen = 1
-	MainNetParams.PubKeyHashAddrID = []byte{0x00}
-	MainNetParams.ScriptHashAddrID = []byte{0x05}
+	MainNetParams.PubKeyHashAddrID = []byte{0x52}
+	MainNetParams.ScriptHashAddrID = []byte{0x07}
 
 	TestNetParams = chaincfg.TestNet3Params
 	TestNetParams.Net = TestnetMagic
 
 	// Address encoding magics
 	TestNetParams.AddressMagicLen = 1
-	TestNetParams.PubKeyHashAddrID = []byte{0x6f}
-	TestNetParams.ScriptHashAddrID = []byte{0xc4}
+	TestNetParams.PubKeyHashAddrID = []byte{0x41}
+	TestNetParams.ScriptHashAddrID = []byte{0xb2}
 
 	RegtestParams = chaincfg.RegressionNetParams
 	RegtestParams.Net = RegtestMagic
@@ -79,6 +83,15 @@ func GetChainParams(chain string) *chaincfg.Params {
 	default:
 		return &MainNetParams
 	}
+}
+
+// GetAddressesFromAddrDesc returns addresses for given address descriptor with flag if the addresses are searchable
+func (p *ZcoinParser) GetAddressesFromAddrDesc(addrDesc bchain.AddressDescriptor) ([]string, bool, error) {
+	if addrDesc[0] == OpZeroCoinMint {
+		return []string{fmt.Sprintf("OP_ZEROCOINMINT %d %s", addrDesc[5], hex.EncodeToString(addrDesc[6:]))}, false, nil
+	}
+
+	return p.OutputScriptToAddressesFunc(addrDesc)
 }
 
 // PackTx packs transaction to byte array using protobuf
