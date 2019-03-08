f89009d7de05d2c2033619fad537089cb6648cc8
iocrypto <dhatton@protonmail.com>
2019-03-08 19:31:05 +0000

    chain sync - blockbook

--- bchain/coins/iocoin/iocoinrpc.go
+++ bchain/coins/iocoin/iocoinrpc.go
@@ -59,14 +59,34 @@ func (b *IocoinRPC) Initialize() error {
 // GetBlock returns block with given hash.
 func (b *IocoinRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
 	var err error
+
+	glog.Info("XXXX GetBlock ",  height)
 	if hash == "" {
 		hash, err = b.GetBlockHash(height)
 		if err != nil {
 			return nil, err
 		}
 	}
-	if !b.ParseBlocks {
-		return b.GetBlockFull(hash)
+	res := btc.ResGetBlockThin{}
+	req := btc.CmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = 1
+	err = b.Call(&req, &res)
+	txs := make([]bchain.Tx, 0, len(res.Result.Txids))
+	for _, txid := range res.Result.Txids {
+		tx, err := b.GetTransaction(txid)
+		if err != nil {
+			if err == bchain.ErrTxNotFound {
+				glog.Errorf("rpc: getblock: skipping transanction in block %s due error: %s", hash, err)
+				continue
+			}
+			return nil, err
+		}
+		txs = append(txs, *tx)
 	}
-	return b.GetBlockWithoutHeader(hash, height)
+	block := &bchain.Block{
+		BlockHeader: res.Result.BlockHeader,
+		Txs:         txs,
+	}
+	return block, nil
 }
