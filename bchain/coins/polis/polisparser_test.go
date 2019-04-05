313c596b7baf0287051bda0a0b4e3afcd3a25143
Cronos <eabz@polispay.org>
2019-04-05 13:10:38 -0600

    Add Parsing Blocks test

--- bchain/coins/polis/polisparser_test.go
+++ bchain/coins/polis/polisparser_test.go
@@ -3,20 +3,106 @@
 package polis
 
 import (
+	"bytes"
+	"fmt"
 	"blockbook/bchain"
 	"blockbook/bchain/coins/btc"
 	"encoding/hex"
+	"io/ioutil"
 	"math/big"
 	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 	"github.com/martinboehm/btcutil/chaincfg"
 )
 
+
+type testBlock struct {
+	size int
+	time int64
+	txs  []string
+}
+
+var testParseBlockTxs = map[int]testBlock{
+	// Simple POW block
+	50000: {
+		size: 1393,
+		time: 1520175937,
+		txs: []string{
+			"b68057244d6ad2df0017bad1cd8a24487e21404b52873c59876a484c93f5a69e",
+			"3d27b82972196ecce604c2923bc9105cd683352fd56f8ba052dee57a296a2d71",
+			"d22704ddad675d652a6b501694d9e026f19b41842946ea285490315cb944c674",
+			"7be2e53414c9480ea1590e40cfa8361ac7594f7435f919a6eeff96367b0dffa9",
+			"e2486a9610698888c4baad7001385e95aca053ab9fc7cc9d15280c9c835c975c",
+		},
+	},
+	// Simple POS block
+	280000: {
+		size: 275,
+		time: 1549070495,
+		txs: []string{
+			"9a820cb226364e852ec5d13bc3ead1ad127bf28ef2808919571200a1262b46b5",
+			"fcca99e281fa0c43085dfe82c24b4367ff21d3a148539e781c061fe29a793ab1",
+		},
+	},
+}
+
 func TestMain(m *testing.M) {
 	c := m.Run()
 	chaincfg.ResetParams()
 	os.Exit(c)
+}
+
+func helperLoadBlock(t *testing.T, height int) []byte {
+	name := fmt.Sprintf("block_dump.%d", height)
+	path := filepath.Join("testdata", name)
+
+	d, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	d = bytes.TrimSpace(d)
+
+	b := make([]byte, hex.DecodedLen(len(d)))
+	_, err = hex.Decode(b, d)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return b
+}
+
+func TestParseBlock(t *testing.T) {
+	p := NewPolisParser(GetChainParams("main"), &btc.Configuration{})
+
+	for height, tb := range testParseBlockTxs {
+		b := helperLoadBlock(t, height)
+
+		blk, err := p.ParseBlock(b)
+		if err != nil {
+			t.Errorf("ParseBlock() error %v", err)
+		}
+
+		if blk.Size != tb.size {
+			t.Errorf("ParseBlock() block size: got %d, want %d", blk.Size, tb.size)
+		}
+
+		if blk.Time != tb.time {
+			t.Errorf("ParseBlock() block time: got %d, want %d", blk.Time, tb.time)
+		}
+
+		if len(blk.Txs) != len(tb.txs) {
+			t.Errorf("ParseBlock() number of transactions: got %d, want %d", len(blk.Txs), len(tb.txs))
+		}
+
+		for ti, tx := range tb.txs {
+			if blk.Txs[ti].Txid != tx {
+				t.Errorf("ParseBlock() transaction %d: got %s, want %s", ti, blk.Txs[ti].Txid, tx)
+			}
+		}
+	}
 }
 
 func Test_GetAddrDescFromAddress_Mainnet(t *testing.T) {
