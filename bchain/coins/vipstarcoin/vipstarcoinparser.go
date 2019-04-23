4dc57213847975b2c20c1cedcad029c3462dd0f4
y-chan <yuto_tetuota@yahoo.co.jp>
2019-04-23 23:03:29 +0900

    fix errors

--- bchain/coins/vipstarcoin/vipstarcoinparser.go
+++ bchain/coins/vipstarcoin/vipstarcoinparser.go
@@ -60,3 +60,86 @@ func GetChainParams(chain string) *chaincfg.Params {
 		return &MainNetParams
 	}
 }
+
+func parseBlockHeader(r io.Reader) (*wire.BlockHeader, error) {
+	h := &wire.BlockHeader{}
+	err := h.Deserialize(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// hash_state_root 32
+	// hash_utxo_root 32
+	// hash_prevout_stake 32
+	// hash_prevout_n 4
+	buf := make([]byte, 100)
+	_, err = io.ReadFull(r, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	sigLength, err := wire.ReadVarInt(r, 0)
+	if err != nil {
+		return nil, err
+	}
+	sigBuf := make([]byte, sigLength)
+	_, err = io.ReadFull(r, sigBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	return h, err
+}
+
+func (p *VIPSTARCOINParser) ParseBlock(b []byte) (*bchain.Block, error) {
+	r := bytes.NewReader(b)
+	w := wire.MsgBlock{}
+
+	h, err := parseBlockHeader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = utils.DecodeTransactions(r, 0, wire.WitnessEncoding, &w)
+	if err != nil {
+		return nil, err
+	}
+
+	txs := make([]bchain.Tx, len(w.Transactions))
+	for ti, t := range w.Transactions {
+		txs[ti] = p.TxFromMsgTx(t, false)
+	}
+
+	return &bchain.Block{
+		BlockHeader: bchain.BlockHeader{
+			Size: len(b),
+			Time: h.Timestamp.Unix(),
+		},
+		Txs: txs,
+	}, nil
+}
+
+// ParseTxFromJson parses JSON message containing transaction and returns Tx struct
+func (p *VIPSTARCOINParser) ParseTxFromJson(msg json.RawMessage) (*bchain.Tx, error) {
+	var tx bchain.Tx
+	err := json.Unmarshal(msg, &tx)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range tx.Vout {
+		vout := &tx.Vout[i]
+		// convert vout.JsonValue to big.Int and clear it, it is only temporary value used for unmarshal
+		vout.ValueSat, err = p.AmountToBigInt(vout.JsonValue)
+		if err != nil {
+			return nil, err
+		}
+		vout.JsonValue = ""
+
+		if vout.ScriptPubKey.Addresses == nil {
+			vout.ScriptPubKey.Addresses = []string{}
+		}
+	}
+
+	return &tx, nil
+}
