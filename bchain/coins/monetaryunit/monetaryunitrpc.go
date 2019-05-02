b1283cf973b6fee7926b55d7dd8160a0e5f5bdc9
Sotiris Blad <sotiris2009@hotmail.com>
2019-05-02 18:38:07 +0300

    fix getblock & tx

--- bchain/coins/monetaryunit/monetaryunitrpc.go
+++ bchain/coins/monetaryunit/monetaryunitrpc.go
@@ -3,9 +3,11 @@ package monetaryunit
 import (
 	"blockbook/bchain"
 	"blockbook/bchain/coins/btc"
+	"encoding/hex"
 	"encoding/json"
 
 	"github.com/golang/glog"
+	"github.com/juju/errors"
 )
 
 // MonetaryUnitRPC is an interface to JSON-RPC bitcoind service.
@@ -60,38 +62,182 @@ func (b *MonetaryUnitRPC) Initialize() error {
 
 // Get Block
 func (b *MonetaryUnitRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
+	var err error
 
-        var err error
-        if hash == "" && height > 0 {
-                hash, err = b.GetBlockHash(height)
-                if err != nil {
-                        return nil, err
-                }
-        }
+	if hash == "" {
+		hash, err = b.GetBlockHash(height)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-        glog.V(1).Info("rpc: getblock (verbosity=1) ", hash)
+	// optimization
+	if height > 0 {
+		return b.GetBlockWithoutHeader(hash, height)
+	}
 
-        res := btc.ResGetBlockThin{}
-        req := btc.CmdGetBlock{Method: "getblock"}
-        req.Params.BlockHash = hash
-        req.Params.Verbosity = 1
-        err = b.Call(&req, &res)
+	header, err := b.GetBlockHeader(hash)
+	if err != nil {
+		return nil, err
+	}
 
-        txs := make([]bchain.Tx, 0, len(res.Result.Txids))
-        for _, txid := range res.Result.Txids {
-                tx, err := b.GetTransaction(txid)
-                if err != nil {
-                        if err == bchain.ErrTxNotFound {
-                                glog.Errorf("rpc: getblock: skipping transanction in block %s due error: %s", hash, err)
-                                continue
-                        }
-                        return nil, err
-                }
-                txs = append(txs, *tx)
-        }
-        block := &bchain.Block{
-                BlockHeader: res.Result.BlockHeader,
-                Txs:         txs,
-        }
-        return block, nil
+	data, err := b.GetBlockRaw(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := b.Parser.ParseBlock(data)
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+
+	block.BlockHeader = *header
+
+	return block, nil
+}
+
+func (b *MonetaryUnitRPC) GetBlockInfo(hash string) (*bchain.BlockInfo, error) {
+	glog.V(1).Info("rpc: getblock (verbosity=true) ", hash)
+
+	res := btc.ResGetBlockInfo{}
+	req := cmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = true
+	err := b.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+	if res.Error != nil {
+		if btc.IsErrBlockNotFound(res.Error) {
+			return nil, bchain.ErrBlockNotFound
+		}
+		return nil, errors.Annotatef(res.Error, "hash %v", hash)
+	}
+	return &res.Result, nil
+}
+
+func (b *MonetaryUnitRPC) GetBlockWithoutHeader(hash string, height uint32) (*bchain.Block, error) {
+	data, err := b.GetBlockRaw(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := b.Parser.ParseBlock(data)
+	if err != nil {
+		return nil, errors.Annotatef(err, "%v %v", height, hash)
+	}
+
+	block.BlockHeader.Hash = hash
+	block.BlockHeader.Height = height
+
+	return block, nil
+}
+
+func (b *MonetaryUnitRPC) GetBlockRaw(hash string) ([]byte, error) {
+	glog.V(1).Info("rpc: getblock (verbosity=false) ", hash)
+
+	res := btc.ResGetBlockRaw{}
+	req := cmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = false
+	err := b.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+	if res.Error != nil {
+		if btc.IsErrBlockNotFound(res.Error) {
+			return nil, bchain.ErrBlockNotFound
+		}
+		return nil, errors.Annotatef(res.Error, "hash %v", hash)
+	}
+	return hex.DecodeString(res.Result)
+}
+
+func (b *MonetaryUnitRPC) GetTransactionForMempool(txid string) (*bchain.Tx, error) {
+	glog.V(1).Info("rpc: getrawtransaction nonverbose ", txid)
+
+	res := btc.ResGetRawTransactionNonverbose{}
+	req := cmdGetRawTransaction{Method: "getrawtransaction"}
+	req.Params.Txid = txid
+	req.Params.Verbose = 0
+	err := b.Call(&req, &res)
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	if res.Error != nil {
+		if btc.IsMissingTx(res.Error) {
+			return nil, bchain.ErrTxNotFound
+		}
+		return nil, errors.Annotatef(res.Error, "txid %v", txid)
+	}
+	data, err := hex.DecodeString(res.Result)
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	tx, err := b.Parser.ParseTx(data)
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	return tx, nil
+}
+
+func (b *MonetaryUnitRPC) GetTransaction(txid string) (*bchain.Tx, error) {
+	r, err := b.getRawTransaction(txid)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := b.Parser.ParseTxFromJson(r)
+	tx.CoinSpecificData = r
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+
+	return tx, nil
+}
+
+func (b *MonetaryUnitRPC) GetTransactionSpecific(tx *bchain.Tx) (json.RawMessage, error) {
+	if csd, ok := tx.CoinSpecificData.(json.RawMessage); ok {
+		return csd, nil
+	}
+	return b.getRawTransaction(tx.Txid)
+}
+
+func (b *MonetaryUnitRPC) getRawTransaction(txid string) (json.RawMessage, error) {
+	glog.V(1).Info("rpc: getrawtransaction ", txid)
+
+	res := btc.ResGetRawTransaction{}
+	req := cmdGetRawTransaction{Method: "getrawtransaction"}
+	req.Params.Txid = txid
+	req.Params.Verbose = 1
+	err := b.Call(&req, &res)
+
+	if err != nil {
+		return nil, errors.Annotatef(err, "txid %v", txid)
+	}
+	if res.Error != nil {
+		if btc.IsMissingTx(res.Error) {
+			return nil, bchain.ErrTxNotFound
+		}
+		return nil, errors.Annotatef(res.Error, "txid %v", txid)
+	}
+	return res.Result, nil
+}
+
+type cmdGetBlock struct {
+	Method string `json:"method"`
+	Params struct {
+		BlockHash string `json:"blockhash"`
+		Verbosity bool   `json:"verbosity"`
+	} `json:"params"`
+}
+
+type cmdGetRawTransaction struct {
+	Method string `json:"method"`
+	Params struct {
+		Txid    string `json:"txid"`
+		Verbose int    `json:"verbose"`
+	} `json:"params"`
 }
