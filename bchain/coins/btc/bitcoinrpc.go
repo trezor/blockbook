b4db9de260099f84fb2d41137fb52661e76bf0fd
Martin Boehm <martin.boehm@1mbsoftware.net>
2019-10-07 18:24:30 +0200

    Add initial commit for fiat rates functionality

--- bchain/coins/btc/bitcoinrpc.go
+++ bchain/coins/btc/bitcoinrpc.go
@@ -55,8 +55,8 @@ type Configuration struct {
 	XPubMagicSegwitP2sh          uint32 `json:"xpub_magic_segwit_p2sh,omitempty"`
 	XPubMagicSegwitNative        uint32 `json:"xpub_magic_segwit_native,omitempty"`
 	Slip44                       uint32 `json:"slip44,omitempty"`
-	AlternativeEstimateFee       string `json:"alternativeEstimateFee,omitempty"`
-	AlternativeEstimateFeeParams string `json:"alternativeEstimateFeeParams,omitempty"`
+	AlternativeEstimateFee       string `json:"alternative_estimate_fee,omitempty"`
+	AlternativeEstimateFeeParams string `json:"alternative_estimate_fee_params,omitempty"`
 	MinimumCoinbaseConfirmations int    `json:"minimumCoinbaseConfirmations,omitempty"`
 }
 
