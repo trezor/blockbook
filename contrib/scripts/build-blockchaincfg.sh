55fa1bd84212c686e4cb588a57f7a579f843ca50
migwi <migwindungu0@gmail.com>
2020-02-23 03:28:44 +0300

    Update the build script to rebuild the binary file on each run

--- contrib/scripts/build-blockchaincfg.sh
+++ contrib/scripts/build-blockchaincfg.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
+# Activate go modules support.
+GO111MODULE='on' 
+
+# build the blockbook binary.
+go build
+
+# generate the config files.
 go run build/templates/generate.go $1 > /dev/null
 mv build/pkg-defs/blockbook/blockchaincfg.json build
 rm -rf build/pkg-defs
