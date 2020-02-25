4137bcaa52cec1cdbadb73ef4e3090a2605caaec
migwi <migwindungu0@gmail.com>
2020-02-25 06:51:03 +0300

    xUnit test runtime

--- build/docker/bin/run_test.sh
+++ build/docker/bin/run_test.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
 
-for i in $(find . -name go.mod -type f -print | grep -v "^./vendor"); do
-  module=$(dirname ${i})
-  echo ${module}
-  (cd ${module} && go test $(TAGS) ./.. $(ARGS))
-done
