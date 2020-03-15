82caf1745325e74ddd6574b58ce92b048658028a
sidhujag <jsidhu@blockchainfoundry.co>
2020-03-15 09:07:37 -0700

    fix parse

--- api/worker.go
+++ api/worker.go
@@ -1014,12 +1014,10 @@ func (w *Worker) AssetAllocationSend(asset string, sender string, reciever strin
 	if err != nil {
 		return nil, err
 	}
-	glog.Warning("hex: ", txAssetSpec.Hex)
-	txAssetSpec.Tx, err = w.chainParser.ParseTxFromJson(json.RawMessage(txAssetSpec.Hex))
+	txAssetSpec.Tx, err = w.chainParser.ParseTxFromJson(jsonRes)
 	if err != nil {
 		return "", err
 	}
-	glog.Warning("Tx: ", txAssetSpec.Tx)
 	txAssetSpec.PrevVouts = make([]*bchain.Vout, len(txAssetSpec.Tx.Vin))
 	for i := range txAssetSpec.Tx.Vin {
 		bchainVin := &txAssetSpec.Tx.Vin[i]
