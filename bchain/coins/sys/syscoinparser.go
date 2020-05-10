4df8db490a39e6109060f783784c874a924e9653
sidhujag <jsidhu@blockchainfoundry.co>
2020-05-10 09:07:34 -0700

    Revert "use varint for asset guid"
    This reverts commit 8b0033e73fdf8f5e4d39438f57eee2c23b22c50e.

--- bchain/coins/sys/syscoinparser.go
+++ bchain/coins/sys/syscoinparser.go
@@ -256,8 +256,8 @@ func (p *SyscoinParser) PackAllocation(a *bchain.AssetAllocationType, buf []byte
 	l := p.BaseParser.PackVaruint(uint(len(a.VoutAssets)), varBuf)
 
 	for k, v := range a.VoutAssets {
-		l = p.BaseParser.PackVaruint(uint(k), varBuf)
-		buf = append(buf, varBuf[:l]...)
+		varBufLE := p.BaseParser.PackUintLE(k)
+		buf = append(buf, varBufLE...)
 
 		l = p.BaseParser.PackVaruint(uint(len(v)), varBuf)
 		buf = append(buf, varBuf[:l]...)
@@ -273,9 +273,8 @@ func (p *SyscoinParser) UnpackAllocation(a *bchain.AssetAllocationType, buf []by
 	numAssets, l := p.BaseParser.UnpackVaruint(buf)
 	a.VoutAssets = make(map[uint32][]bchain.AssetOutType, numAssets)
 	for i := 0; i < int(numAssets); i++ {
-		guid, ll := p.BaseParser.UnpackVaruint(buf[l:])
-		l += ll
-		assetGuid = uint32(guid)
+		assetGuid := p.BaseParser.UnpackUintLE(buf[l:])
+		l += 4
 		numOutputs, ll := p.BaseParser.UnpackVaruint(buf[l:])
 		l += ll
 		assetOutArray, ok := a.VoutAssets[assetGuid]
@@ -423,7 +422,7 @@ func (p *SyscoinParser) UnpackMintSyscoin(a *bchain.MintSyscoinType, buf []byte)
 }
 
 func (p *SyscoinParser) AppendMintSyscoin(a *bchain.MintSyscoinType, buf []byte) []byte {
-	varBuf := make([]byte, 512)
+	varBuf := make([]byte, 4096)
 	buf = p.PackAllocation(&a.Allocation, buf)
 
 	l := p.BaseParser.PackVaruint(uint(a.BridgeTransferId), varBuf)
@@ -485,8 +484,8 @@ func (p *SyscoinParser) GetAllocationFromTx(tx *bchain.Tx) (*bchain.AssetAllocat
 	}
 	var assetAllocation bchain.AssetAllocationType
 	l := p.UnpackAllocation(&assetAllocation, sptData)
-	// should be atleast 5 bytes minimum
-	if l < 5 {
+	// should be atleast 8 bytes minimum
+	if l < 8 {
 		return nil, errors.New("Could not decode asset allocation")
 	}
 	return &assetAllocation, nil
