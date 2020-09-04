83b08177904994b2110c862bc7f97558a3ed8ebd
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-04 12:13:50 -0700

    fix test

--- db/rocksdb_syscointype_test.go
+++ db/rocksdb_syscointype_test.go
@@ -449,6 +449,12 @@ func TestRocksDB_Index_SyscoinType(t *testing.T) {
 	verifyAfterSyscoinTypeBlock2(t, d)
 	if err := checkColumn(d, cfBlockTxs, []keyPair{
 		{
+			"00000071",
+			dbtestdata.TxidS2T0 + "01" + "0000000000000000000000000000000000000000000000000000000000000000" + varintToHex(0) +
+			dbtestdata.TxidS2T1 + "01" + dbtestdata.TxidS1T1 + varuintToHex(1),
+			nil,
+		},
+		{
 			"00000070",
 			dbtestdata.TxidS1T0 + "01" + "0000000000000000000000000000000000000000000000000000000000000000" + varintToHex(0) +
 			dbtestdata.TxidS1T1 + "01" + dbtestdata.TxidS1T1INPUT0 + varuintToHex(0),
@@ -591,12 +597,6 @@ func Test_BulkConnect_SyscoinType(t *testing.T) {
 			"00000071",
 			dbtestdata.TxidS2T0 + "01" + "0000000000000000000000000000000000000000000000000000000000000000" + varintToHex(0) +
 			dbtestdata.TxidS2T1 + "01" + dbtestdata.TxidS1T1 + varintToHex(1),
-			nil,
-		},
-		{
-			"00000070",
-			dbtestdata.TxidS1T0 + "01" + "0000000000000000000000000000000000000000000000000000000000000000" + varintToHex(0) +
-			dbtestdata.TxidS1T1 + "01" + dbtestdata.TxidS1T1INPUT0 + varintToHex(0),
 			nil,
 		},
 	}); err != nil {
