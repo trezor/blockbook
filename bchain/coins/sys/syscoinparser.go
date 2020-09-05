130ba7c1d624707bc5194aa78339fab0939abbe1
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-05 12:08:24 -0700

    adjust cap

--- bchain/coins/sys/syscoinparser.go
+++ bchain/coins/sys/syscoinparser.go
@@ -609,7 +609,7 @@ func (p *SyscoinParser) PackTxIndexes(txi []bchain.TxIndexes) []byte {
 
 func (p *SyscoinParser) PackAsset(asset *bchain.Asset) ([]byte, error) {
 	buf := make([]byte, 0, 52)
-	varBuf := make([]byte, 40)
+	varBuf := make([]byte, 4)3w
 	l := p.BaseParser.PackVaruint(uint(asset.Transactions), varBuf)
 	buf = append(buf, varBuf[:l]...)
 	var buffer bytes.Buffer
