8fb8eb3dbd2e8b70c367083dd8552dc2589fef38
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-04 21:27:12 -0700

    fix test

--- db/rocksdb_syscointype_test.go
+++ db/rocksdb_syscointype_test.go
@@ -459,13 +459,13 @@ func TestRocksDB_Index_SyscoinType(t *testing.T) {
 		{
 			"00000071",
 			dbtestdata.TxidS2T0 + "01" + "0000000000000000000000000000000000000000000000000000000000000000" + varintToHex(0) +
-			dbtestdata.TxidS2T1 + "01" + dbtestdata.TxidS1T1 + varuintToHex(1),
+			dbtestdata.TxidS2T1 + "01" + dbtestdata.TxidS1T1 + varintToHex(1),
 			nil,
 		},
 		{
 			"00000070",
 			dbtestdata.TxidS1T0 + "01" + "0000000000000000000000000000000000000000000000000000000000000000" + varintToHex(0) +
-			dbtestdata.TxidS1T1 + "01" + dbtestdata.TxidS1T1INPUT0 + varuintToHex(0),
+			dbtestdata.TxidS1T1 + "01" + dbtestdata.TxidS1T1INPUT0 + varintToHex(0),
 			nil,
 		},
 	}); err != nil {
