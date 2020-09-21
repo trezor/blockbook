994ae5dcdce4be4ff1818547800f7fccc5729549
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-21 07:20:59 -0700

    fix test

--- db/rocksdb_syscointype_test.go
+++ db/rocksdb_syscointype_test.go
@@ -217,8 +217,8 @@ func verifyAfterSyscoinTypeBlock2(t *testing.T, d *RocksDB) {
 	if dBAsset.Transactions != 2 {
 		t.Fatal(fmt.Sprint("Block2: Property mismatch dbAsset.Transaction: ", dBAsset.Transactions, ". Expected: 2"))
 	}
-	if string(dBAsset.AssetObj.Symbol) != "" {
-		t.Fatal(fmt.Sprint("Block2: Property mismatch dBAsset.AssetObj.Symbol: ", string(dBAsset.AssetObj.Symbol) , ". Expected: ''"))
+	if string(dBAsset.AssetObj.Symbol) != base64.StdEncoding.EncodeToString([]byte("CAT")) {
+		t.Fatal(fmt.Sprint("Block1: Property mismatch dBAsset.AssetObj.Symbol: ", string(dBAsset.AssetObj.Symbol) , ". Expected: " + base64.StdEncoding.EncodeToString([]byte("CAT"))))
 	}
 	pubdata := "{\"desc\":\"" + base64.StdEncoding.EncodeToString([]byte("new publicvalue")) + "\"}"
 	if !bytes.Equal(dBAsset.AssetObj.PubData, []byte(pubdata)) {
