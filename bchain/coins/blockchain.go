6a9852c4631cf3068f7d745ab4cae7905fdb8d8b
gruve-p <jackielove4u@hotmail.com>
2021-09-21 13:33:04 +0200

    feat: add groestlcoin regtest mode

--- bchain/coins/blockchain.go
+++ bchain/coins/blockchain.go
@@ -97,6 +97,7 @@ func init() {
 	BlockChainFactories["Liquid"] = liquid.NewLiquidRPC
 	BlockChainFactories["Groestlcoin"] = grs.NewGroestlcoinRPC
 	BlockChainFactories["Groestlcoin Testnet"] = grs.NewGroestlcoinRPC
+	BlockChainFactories["Groestlcoin Regtest"] = grs.NewGroestlcoinRPC
 	BlockChainFactories["PIVX"] = pivx.NewPivXRPC
 	BlockChainFactories["PIVX Testnet"] = pivx.NewPivXRPC
 	BlockChainFactories["Polis"] = polis.NewPolisRPC
