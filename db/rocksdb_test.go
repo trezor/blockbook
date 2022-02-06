87b2b5e921d85354190c16f77a12f7029a986690
Martin Boehm <martin.boehm@1mbsoftware.net>
2022-02-06 21:57:54 +0100

    Minor refactor

--- db/rocksdb_test.go
+++ db/rocksdb_test.go
@@ -12,7 +12,6 @@ import (
 	"sort"
 	"strings"
 	"testing"
-	"time"
 
 	vlq "github.com/bsm/go-vlq"
 	"github.com/juju/errors"
@@ -1480,81 +1479,5 @@ func Test_reorderUtxo(t *testing.T) {
 				t.Errorf("reorderUtxo %s, got %+v, want %+v", tt.name, tt.utxos, tt.want)
 			}
 		})
-	}
-}
-
-func TestRocksTickers(t *testing.T) {
-	d := setupRocksDB(t, &testBitcoinParser{
-		BitcoinParser: bitcoinTestnetParser(),
-	})
-	defer closeAndDestroyRocksDB(t, d)
-
-	// Test valid formats
-	for _, date := range []string{"20190130", "2019013012", "201901301250", "20190130125030"} {
-		_, err := FiatRatesConvertDate(date)
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-	}
-
-	// Test invalid formats
-	for _, date := range []string{"01102019", "10201901", "", "abc", "20190130xxx"} {
-		_, err := FiatRatesConvertDate(date)
-		if err == nil {
-			t.Errorf("Wrongly-formatted date \"%v\" marked as valid!", date)
-		}
-	}
-
-	// Test storing & finding tickers
-	key, _ := time.Parse(FiatRatesTimeFormat, "20190627000000")
-	futureKey, _ := time.Parse(FiatRatesTimeFormat, "20190630000000")
-
-	ts1, _ := time.Parse(FiatRatesTimeFormat, "20190628000000")
-	ticker1 := &CurrencyRatesTicker{
-		Timestamp: &ts1,
-		Rates: map[string]float64{
-			"usd": 20000,
-		},
-	}
-
-	ts2, _ := time.Parse(FiatRatesTimeFormat, "20190629000000")
-	ticker2 := &CurrencyRatesTicker{
-		Timestamp: &ts2,
-		Rates: map[string]float64{
-			"usd": 30000,
-		},
-	}
-	err := d.FiatRatesStoreTicker(ticker1)
-	if err != nil {
-		t.Errorf("Error storing ticker! %v", err)
-	}
-	d.FiatRatesStoreTicker(ticker2)
-	if err != nil {
-		t.Errorf("Error storing ticker! %v", err)
-	}
-
-	ticker, err := d.FiatRatesFindTicker(&key) // should find the closest key (ticker1)
-	if err != nil {
-		t.Errorf("TestRocksTickers err: %+v", err)
-	} else if ticker == nil {
-		t.Errorf("Ticker not found")
-	} else if ticker.Timestamp.Format(FiatRatesTimeFormat) != ticker1.Timestamp.Format(FiatRatesTimeFormat) {
-		t.Errorf("Incorrect ticker found. Expected: %v, found: %+v", ticker1.Timestamp, ticker.Timestamp)
-	}
-
-	ticker, err = d.FiatRatesFindLastTicker() // should find the last key (ticker2)
-	if err != nil {
-		t.Errorf("TestRocksTickers err: %+v", err)
-	} else if ticker == nil {
-		t.Errorf("Ticker not found")
-	} else if ticker.Timestamp.Format(FiatRatesTimeFormat) != ticker2.Timestamp.Format(FiatRatesTimeFormat) {
-		t.Errorf("Incorrect ticker found. Expected: %v, found: %+v", ticker1.Timestamp, ticker.Timestamp)
-	}
-
-	ticker, err = d.FiatRatesFindTicker(&futureKey) // should not find anything
-	if err != nil {
-		t.Errorf("TestRocksTickers err: %+v", err)
-	} else if ticker != nil {
-		t.Errorf("Ticker found, but the timestamp is older than the last ticker entry.")
 	}
 }
