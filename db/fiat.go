e0be8aa4001ee33f72c885c7aca2bdd2b7f58432
Martin Boehm <martin.boehm@1mbsoftware.net>
2022-06-15 09:32:48 +0200

    Fiat rates refactor, fetch rates for tokens

--- db/fiat.go
+++ db/fiat.go
@@ -1,9 +1,13 @@
 package db
 
 import (
-	"encoding/json"
+	"encoding/binary"
+	"math"
+	"sync"
 	"time"
 
+	vlq "github.com/bsm/go-vlq"
+	"github.com/flier/gorocksdb"
 	"github.com/golang/glog"
 	"github.com/juju/errors"
 )
@@ -11,29 +15,83 @@ import (
 // FiatRatesTimeFormat is a format string for storing FiatRates timestamps in rocksdb
 const FiatRatesTimeFormat = "20060102150405" // YYYYMMDDhhmmss
 
+var tickersMux sync.Mutex
+var lastTickerInDB *CurrencyRatesTicker
+var currentTicker *CurrencyRatesTicker
+
 // CurrencyRatesTicker contains coin ticker data fetched from API
 type CurrencyRatesTicker struct {
-	Timestamp *time.Time // return as unix timestamp in API
-	Rates     map[string]float64
+	Timestamp  time.Time          // return as unix timestamp in API
+	Rates      map[string]float32 // rates of the base currency against a list of vs currencies
+	TokenRates map[string]float32 // rates of the tokens (identified by the address of the contract) against the base currency
 }
 
-// ResultTickerAsString contains formatted CurrencyRatesTicker data
-type ResultTickerAsString struct {
-	Timestamp int64              `json:"ts,omitempty"`
-	Rates     map[string]float64 `json:"rates"`
-	Error     string             `json:"error,omitempty"`
+func packTimestamp(t *time.Time) []byte {
+	return []byte(t.UTC().Format(FiatRatesTimeFormat))
 }
 
-// ResultTickersAsString contains a formatted CurrencyRatesTicker list
-type ResultTickersAsString struct {
-	Tickers []ResultTickerAsString `json:"tickers"`
+func packFloat32(buf []byte, n float32) int {
+	binary.BigEndian.PutUint32(buf, math.Float32bits(n))
+	return 4
 }
 
-// ResultTickerListAsString contains formatted data about available currency tickers
-type ResultTickerListAsString struct {
-	Timestamp int64    `json:"ts,omitempty"`
-	Tickers   []string `json:"available_currencies"`
-	Error     string   `json:"error,omitempty"`
+func unpackFloat32(buf []byte) (float32, int) {
+	return math.Float32frombits(binary.BigEndian.Uint32(buf)), 4
+}
+
+func packCurrencyRatesTicker(ticker *CurrencyRatesTicker) []byte {
+	buf := make([]byte, 0, 32)
+	varBuf := make([]byte, vlq.MaxLen64)
+	l := packVaruint(uint(len(ticker.Rates)), varBuf)
+	buf = append(buf, varBuf[:l]...)
+	for c, v := range ticker.Rates {
+		buf = append(buf, packString(c)...)
+		l = packFloat32(varBuf, v)
+		buf = append(buf, varBuf[:l]...)
+	}
+	l = packVaruint(uint(len(ticker.TokenRates)), varBuf)
+	buf = append(buf, varBuf[:l]...)
+	for c, v := range ticker.TokenRates {
+		buf = append(buf, packString(c)...)
+		l = packFloat32(varBuf, v)
+		buf = append(buf, varBuf[:l]...)
+	}
+	return buf
+}
+
+func unpackCurrencyRatesTicker(buf []byte) (*CurrencyRatesTicker, error) {
+	var (
+		ticker CurrencyRatesTicker
+		s      string
+		l      int
+		len    uint
+		v      float32
+	)
+	len, l = unpackVaruint(buf)
+	buf = buf[l:]
+	if len > 0 {
+		ticker.Rates = make(map[string]float32, len)
+		for i := 0; i < int(len); i++ {
+			s, l = unpackString(buf)
+			buf = buf[l:]
+			v, l = unpackFloat32(buf)
+			buf = buf[l:]
+			ticker.Rates[s] = v
+		}
+	}
+	len, l = unpackVaruint(buf)
+	buf = buf[l:]
+	if len > 0 {
+		ticker.TokenRates = make(map[string]float32, len)
+		for i := 0; i < int(len); i++ {
+			s, l = unpackString(buf)
+			buf = buf[l:]
+			v, l = unpackFloat32(buf)
+			buf = buf[l:]
+			ticker.TokenRates[s] = v
+		}
+	}
+	return &ticker, nil
 }
 
 // FiatRatesConvertDate checks if the date is in correct format and returns the Time object.
@@ -51,85 +109,131 @@ func FiatRatesConvertDate(date string) (*time.Time, error) {
 }
 
 // FiatRatesStoreTicker stores ticker data at the specified time
-func (d *RocksDB) FiatRatesStoreTicker(ticker *CurrencyRatesTicker) error {
+func (d *RocksDB) FiatRatesStoreTicker(wb *gorocksdb.WriteBatch, ticker *CurrencyRatesTicker) error {
 	if len(ticker.Rates) == 0 {
 		return errors.New("Error storing ticker: empty rates")
-	} else if ticker.Timestamp == nil {
-		return errors.New("Error storing ticker: empty timestamp")
 	}
-	ratesMarshalled, err := json.Marshal(ticker.Rates)
-	if err != nil {
-		glog.Error("Error marshalling ticker rates: ", err)
-		return err
-	}
-	timeFormatted := ticker.Timestamp.UTC().Format(FiatRatesTimeFormat)
-	err = d.db.PutCF(d.wo, d.cfh[cfFiatRates], []byte(timeFormatted), ratesMarshalled)
-	if err != nil {
-		glog.Error("Error storing ticker: ", err)
-		return err
-	}
+	wb.PutCF(d.cfh[cfFiatRates], packTimestamp(&ticker.Timestamp), packCurrencyRatesTicker(ticker))
 	return nil
 }
 
-// FiatRatesFindTicker gets FiatRates data closest to the specified timestamp
-func (d *RocksDB) FiatRatesFindTicker(tickerTime *time.Time) (*CurrencyRatesTicker, error) {
-	ticker := &CurrencyRatesTicker{}
+func getTickerFromIterator(it *gorocksdb.Iterator, vsCurrency string, token string) (*CurrencyRatesTicker, error) {
+	timeObj, err := time.Parse(FiatRatesTimeFormat, string(it.Key().Data()))
+	if err != nil {
+		return nil, err
+	}
+	ticker, err := unpackCurrencyRatesTicker(it.Value().Data())
+	if err != nil {
+		return nil, err
+	}
+	if vsCurrency != "" {
+		if ticker.Rates == nil {
+			return nil, nil
+		}
+		if _, found := ticker.Rates[vsCurrency]; !found {
+			return nil, nil
+		}
+	}
+	if token != "" {
+		if ticker.TokenRates == nil {
+			return nil, nil
+		}
+		if _, found := ticker.TokenRates[token]; !found {
+			return nil, nil
+		}
+	}
+	ticker.Timestamp = timeObj.UTC()
+	return ticker, nil
+}
+
+// FiatRatesGetTicker gets FiatRates ticker at the specified timestamp if it exist
+func (d *RocksDB) FiatRatesGetTicker(tickerTime *time.Time) (*CurrencyRatesTicker, error) {
+	tickerTimeFormatted := tickerTime.UTC().Format(FiatRatesTimeFormat)
+	val, err := d.db.GetCF(d.ro, d.cfh[cfFiatRates], []byte(tickerTimeFormatted))
+	if err != nil {
+		return nil, err
+	}
+	defer val.Free()
+	data := val.Data()
+	if len(data) == 0 {
+		return nil, nil
+	}
+	ticker, err := unpackCurrencyRatesTicker(data)
+	if err != nil {
+		return nil, err
+	}
+	ticker.Timestamp = tickerTime.UTC()
+	return ticker, nil
+}
+
+// FiatRatesFindTicker gets FiatRates data closest to the specified timestamp, of the base currency, vsCurrency or the token if specified
+func (d *RocksDB) FiatRatesFindTicker(tickerTime *time.Time, vsCurrency string, token string) (*CurrencyRatesTicker, error) {
+	tickersMux.Lock()
+	if currentTicker != nil && lastTickerInDB != nil {
+		if tickerTime.After(lastTickerInDB.Timestamp) {
+			f := true
+			if token != "" && currentTicker.TokenRates != nil {
+				_, f = currentTicker.TokenRates[token]
+			}
+			if f {
+				tickersMux.Unlock()
+				return currentTicker, nil
+			}
+		}
+	}
+	tickersMux.Unlock()
+
 	tickerTimeFormatted := tickerTime.UTC().Format(FiatRatesTimeFormat)
 	it := d.db.NewIteratorCF(d.ro, d.cfh[cfFiatRates])
 	defer it.Close()
 
 	for it.Seek([]byte(tickerTimeFormatted)); it.Valid(); it.Next() {
-		timeObj, err := time.Parse(FiatRatesTimeFormat, string(it.Key().Data()))
+		ticker, err := getTickerFromIterator(it, vsCurrency, token)
 		if err != nil {
-			glog.Error("FiatRatesFindTicker time parse error: ", err)
+			glog.Error("FiatRatesFindTicker error: ", err)
 			return nil, err
 		}
-		timeObj = timeObj.UTC()
-		ticker.Timestamp = &timeObj
-		err = json.Unmarshal(it.Value().Data(), &ticker.Rates)
-		if err != nil {
-			glog.Error("FiatRatesFindTicker error unpacking rates: ", err)
-			return nil, err
+		if ticker != nil {
+			return ticker, nil
 		}
-		break
 	}
-	if err := it.Err(); err != nil {
-		glog.Error("FiatRatesFindTicker Iterator error: ", err)
-		return nil, err
-	}
-	if !it.Valid() {
-		return nil, nil // ticker not found
-	}
-	return ticker, nil
+	return nil, nil
 }
 
-// FiatRatesFindLastTicker gets the last FiatRates record
-func (d *RocksDB) FiatRatesFindLastTicker() (*CurrencyRatesTicker, error) {
-	ticker := &CurrencyRatesTicker{}
+// FiatRatesFindLastTicker gets the last FiatRates record, of the base currency, vsCurrency or the token if specified
+func (d *RocksDB) FiatRatesFindLastTicker(vsCurrency string, token string) (*CurrencyRatesTicker, error) {
 	it := d.db.NewIteratorCF(d.ro, d.cfh[cfFiatRates])
 	defer it.Close()
 
-	for it.SeekToLast(); it.Valid(); it.Next() {
-		timeObj, err := time.Parse(FiatRatesTimeFormat, string(it.Key().Data()))
+	for it.SeekToLast(); it.Valid(); it.Prev() {
+		ticker, err := getTickerFromIterator(it, vsCurrency, token)
 		if err != nil {
-			glog.Error("FiatRatesFindTicker time parse error: ", err)
+			glog.Error("FiatRatesFindLastTicker error: ", err)
 			return nil, err
 		}
-		timeObj = timeObj.UTC()
-		ticker.Timestamp = &timeObj
-		err = json.Unmarshal(it.Value().Data(), &ticker.Rates)
-		if err != nil {
-			glog.Error("FiatRatesFindTicker error unpacking rates: ", err)
-			return nil, err
+		if ticker != nil {
+			// if without filter, store the ticker for later use
+			if vsCurrency == "" && token == "" {
+				tickersMux.Lock()
+				lastTickerInDB = ticker
+				tickersMux.Unlock()
+			}
+			return ticker, nil
 		}
-		break
 	}
-	if err := it.Err(); err != nil {
-		glog.Error("FiatRatesFindLastTicker Iterator error: ", err)
-		return ticker, err
-	}
-	if !it.Valid() {
-		return nil, nil // ticker not found
-	}
-	return ticker, nil
+	return nil, nil
+}
+
+// FiatRatesGetCurrentTicker return current ticker
+func (d *RocksDB) FiatRatesGetCurrentTicker(tickerTime *time.Time, token string) (*CurrencyRatesTicker, error) {
+	tickersMux.Lock()
+	defer tickersMux.Unlock()
+	return currentTicker, nil
+}
+
+// FiatRatesCurrentTicker return current ticker
+func (d *RocksDB) FiatRatesSetCurrentTicker(t *CurrencyRatesTicker) {
+	tickersMux.Lock()
+	defer tickersMux.Unlock()
+	currentTicker = t
 }
--- fiat/fiat_rates.go
+++ fiat/fiat_rates.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"reflect"
+	"math/rand"
 	"time"
 
 	"github.com/golang/glog"
@@ -16,28 +16,29 @@ type OnNewFiatRatesTicker func(ticker *db.CurrencyRatesTicker)
 
 // RatesDownloaderInterface provides method signatures for specific fiat rates downloaders
 type RatesDownloaderInterface interface {
-	getTicker(timestamp *time.Time) (*db.CurrencyRatesTicker, error)
-	marketDataExists(timestamp *time.Time) (bool, error)
+	CurrentTickers() (*db.CurrencyRatesTicker, error)
+	UpdateHistoricalTickers() error
+	UpdateHistoricalTokenTickers() error
 }
 
 // RatesDownloader stores FiatRates API parameters
 type RatesDownloader struct {
-	periodSeconds       time.Duration
+	periodSeconds       int64
 	db                  *db.RocksDB
-	startTime           *time.Time // a starting timestamp for tests to be deterministic (time.Now() for production)
 	timeFormat          string
 	callbackOnNewTicker OnNewFiatRatesTicker
 	downloader          RatesDownloaderInterface
 }
 
 // NewFiatRatesDownloader initializes the downloader for FiatRates API.
-// If the startTime is nil, the downloader will start from the beginning.
-func NewFiatRatesDownloader(db *db.RocksDB, apiType string, params string, startTime *time.Time, callback OnNewFiatRatesTicker) (*RatesDownloader, error) {
+func NewFiatRatesDownloader(db *db.RocksDB, apiType string, params string, callback OnNewFiatRatesTicker) (*RatesDownloader, error) {
 	var rd = &RatesDownloader{}
 	type fiatRatesParams struct {
-		URL           string `json:"url"`
-		Coin          string `json:"coin"`
-		PeriodSeconds int    `json:"periodSeconds"`
+		URL                string `json:"url"`
+		Coin               string `json:"coin"`
+		PlatformIdentifier string `json:"platformIdentifier"`
+		PlatformVsCurrency string `json:"platformVsCurrency"`
+		PeriodSeconds      int64  `json:"periodSeconds"`
 	}
 	rdParams := &fiatRatesParams{}
 	err := json.Unmarshal([]byte(params), &rdParams)
@@ -47,168 +48,62 @@ func NewFiatRatesDownloader(db *db.RocksDB, apiType string, params string, start
 	if rdParams.URL == "" || rdParams.PeriodSeconds == 0 {
 		return nil, errors.New("Missing parameters")
 	}
-	rd.timeFormat = "02-01-2006"                                           // Layout string for FiatRates date formatting (DD-MM-YYYY)
-	rd.periodSeconds = time.Duration(rdParams.PeriodSeconds) * time.Second // Time period for syncing the latest market data
+	rd.timeFormat = "02-01-2006"              // Layout string for FiatRates date formatting (DD-MM-YYYY)
+	rd.periodSeconds = rdParams.PeriodSeconds // Time period for syncing the latest market data
+	if rd.periodSeconds < 60 {                // minimum is one minute
+		rd.periodSeconds = 60
+	}
 	rd.db = db
 	rd.callbackOnNewTicker = callback
-	if startTime == nil {
-		timeNow := time.Now().UTC()
-		rd.startTime = &timeNow
-	} else {
-		rd.startTime = startTime // If startTime is nil, time.Now() will be used
-	}
 	if apiType == "coingecko" {
-		rd.downloader = NewCoinGeckoDownloader(rdParams.URL, rdParams.Coin, rd.timeFormat)
+		throttlingDelayMs := 50
+		if callback == nil {
+			// a small hack - in tests the callback is not used, therefore there is no delay slowing the test
+			throttlingDelayMs = 0
+		}
+		rd.downloader = NewCoinGeckoDownloader(db, rdParams.URL, rdParams.Coin, rdParams.PlatformIdentifier, rdParams.PlatformVsCurrency, rd.timeFormat, throttlingDelayMs)
 	} else {
 		return nil, fmt.Errorf("NewFiatRatesDownloader: incorrect API type %q", apiType)
 	}
 	return rd, nil
 }
 
-// Run starts the FiatRates downloader. If there are tickers available, it continues from the last record.
-// If there are no tickers, it finds the earliest market data available on API and downloads historical data.
-// When historical data is downloaded, it continues to fetch the latest ticker prices.
+// Run periodically downloads current (every 15 minutes) and historical (once a day) tickers
 func (rd *RatesDownloader) Run() error {
-	var timestamp *time.Time
+	var lastHistoricalTickers time.Time
 
-	// Check if there are any tickers stored in database
-	glog.Infof("Finding last available ticker...")
-	ticker, err := rd.db.FiatRatesFindLastTicker()
-	if err != nil {
-		glog.Errorf("RatesDownloader FindTicker error: %v", err)
-		return err
-	}
-
-	if ticker == nil {
-		// If no tickers found, start downloading from the beginning
-		glog.Infof("No tickers found! Looking up the earliest market data available on API and downloading from there.")
-		timestamp, err = rd.findEarliestMarketData()
-		if err != nil {
-			glog.Errorf("Error looking up earliest market data: %v", err)
-			return err
-		}
-	} else {
-		// If found, continue downloading data from the next day of the last available record
-		glog.Infof("Last available ticker: %v", ticker.Timestamp)
-		timestamp = ticker.Timestamp
-	}
-	err = rd.syncHistorical(timestamp)
-	if err != nil {
-		glog.Errorf("RatesDownloader syncHistorical error: %v", err)
-		return err
-	}
-	if err := rd.syncLatest(); err != nil {
-		glog.Errorf("RatesDownloader syncLatest error: %v", err)
-		return err
-	}
-	return nil
-}
-
-// FindEarliestMarketData uses binary search to find the oldest market data available on API.
-func (rd *RatesDownloader) findEarliestMarketData() (*time.Time, error) {
-	minDateString := "03-01-2009"
-	minDate, err := time.Parse(rd.timeFormat, minDateString)
-	if err != nil {
-		glog.Error("Error parsing date: ", err)
-		return nil, err
-	}
-	maxDate := rd.startTime.Add(time.Duration(-24) * time.Hour) // today's historical tickers may not be ready yet, so set to yesterday
-	currentDate := maxDate
 	for {
-		var dataExists bool = false
-		for {
-			dataExists, err = rd.downloader.marketDataExists(&currentDate)
-			if err != nil {
-				glog.Errorf("Error checking if market data exists for date %v. Error: %v. Retrying in %v seconds.", currentDate, err, rd.periodSeconds)
-				timer := time.NewTimer(rd.periodSeconds)
-				<-timer.C
-			}
-			break
-		}
-		dateDiff := currentDate.Sub(minDate)
-		if dataExists {
-			if dateDiff < time.Hour*24 {
-				maxDate := time.Date(maxDate.Year(), maxDate.Month(), maxDate.Day(), 0, 0, 0, 0, maxDate.Location()) // truncate time to day
-				return &maxDate, nil
-			}
-			maxDate = currentDate
-			currentDate = currentDate.Add(-1 * dateDiff / 2)
+		tickers, err := rd.downloader.CurrentTickers()
+		if err != nil && tickers != nil {
+			glog.Error("FiatRatesDownloader: CurrentTickers error ", err)
 		} else {
-			minDate = currentDate
-			currentDate = currentDate.Add(maxDate.Sub(currentDate) / 2)
+			rd.db.FiatRatesSetCurrentTicker(tickers)
+			glog.Info("FiatRatesDownloader: CurrentTickers updated")
 		}
+		if time.Now().UTC().YearDay() != lastHistoricalTickers.YearDay() || time.Now().UTC().Year() != lastHistoricalTickers.Year() {
+			err = rd.downloader.UpdateHistoricalTickers()
+			if err != nil {
+				glog.Error("FiatRatesDownloader: UpdateHistoricalTickers error ", err)
+			} else {
+				lastHistoricalTickers = time.Now().UTC()
+				glog.Info("FiatRatesDownloader: UpdateHistoricalTickers finished")
+			}
+			// UpdateHistoricalTokenTickers in a goroutine, it can take quite some time as there may be many tokens
+			go func() {
+				err := rd.downloader.UpdateHistoricalTokenTickers()
+				if err != nil {
+					glog.Error("FiatRatesDownloader: UpdateHistoricalTokenTickers error ", err)
+				} else {
+					lastHistoricalTickers = time.Now().UTC()
+					glog.Info("FiatRatesDownloader: UpdateHistoricalTokenTickers finished")
+				}
+			}()
+		}
+		// next run on the
+		now := time.Now().Unix()
+		next := now + rd.periodSeconds
+		next -= next % rd.periodSeconds
+		next += int64(rand.Intn(12))
+		time.Sleep(time.Duration(next-now) * time.Second)
 	}
-}
-
-// syncLatest downloads the latest FiatRates data every rd.PeriodSeconds
-func (rd *RatesDownloader) syncLatest() error {
-	timer := time.NewTimer(rd.periodSeconds)
-	var lastTickerRates map[string]float64
-	sameTickerCounter := 0
-	for {
-		ticker, err := rd.downloader.getTicker(nil)
-		if err != nil {
-			// Do not exit on GET error, log it, wait and try again
-			glog.Errorf("syncLatest GetData error: %v", err)
-			<-timer.C
-			timer.Reset(rd.periodSeconds)
-			continue
-		}
-
-		if sameTickerCounter < 5 && reflect.DeepEqual(ticker.Rates, lastTickerRates) {
-			// If rates are the same as previous, do not store them
-			glog.Infof("syncLatest: ticker rates for %v are the same as previous, skipping...", ticker.Timestamp)
-			<-timer.C
-			timer.Reset(rd.periodSeconds)
-			sameTickerCounter++
-			continue
-		}
-		lastTickerRates = ticker.Rates
-		sameTickerCounter = 0
-
-		glog.Infof("syncLatest: storing ticker for %v", ticker.Timestamp)
-		err = rd.db.FiatRatesStoreTicker(ticker)
-		if err != nil {
-			// If there's an error storing ticker (like missing rates), log it, wait and try again
-			glog.Errorf("syncLatest StoreTicker error: %v", err)
-		} else if rd.callbackOnNewTicker != nil {
-			rd.callbackOnNewTicker(ticker)
-		}
-		<-timer.C
-		timer.Reset(rd.periodSeconds)
-	}
-}
-
-// syncHistorical downloads all the historical data since the specified timestamp till today,
-// then continues to download the latest rates
-func (rd *RatesDownloader) syncHistorical(timestamp *time.Time) error {
-	period := time.Duration(1) * time.Second
-	timer := time.NewTimer(period)
-	for {
-		if rd.startTime.Sub(*timestamp) < time.Duration(time.Hour*24) {
-			break
-		}
-
-		ticker, err := rd.downloader.getTicker(timestamp)
-		if err != nil {
-			// Do not exit on GET error, log it, wait and try again
-			glog.Errorf("syncHistorical GetData error: %v", err)
-			<-timer.C
-			timer.Reset(rd.periodSeconds)
-			continue
-		}
-
-		glog.Infof("syncHistorical: storing ticker for %v", ticker.Timestamp)
-		err = rd.db.FiatRatesStoreTicker(ticker)
-		if err != nil {
-			// If there's an error storing ticker (like missing rates), log it and continue to the next day
-			glog.Errorf("syncHistorical error storing ticker for %v: %v", timestamp, err)
-		}
-
-		*timestamp = timestamp.Add(time.Hour * 24) // go to the next day
-
-		<-timer.C
-		timer.Reset(period)
-	}
-	return nil
 }
