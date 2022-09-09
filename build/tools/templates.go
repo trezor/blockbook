2904161236fccbd7a177e695f1f01e378c230bee
Alexandr Matyushkin <47747143+matyushkins@users.noreply.github.com>
2022-09-09 14:16:49 +0300

    Master (#137)
    * feat: edit Dockerfile for arm64 build compatibility
    
    * Enable arm64 and MacOS Docker build
    
    * dash (+testnet) 0.17.0.3 → 0.18.0.1
    
    * firo 0.14.10.0 → 0.14.11.1

--- build/tools/templates.go
+++ build/tools/templates.go
@@ -8,9 +8,35 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"reflect"
+	"runtime"
 	"text/template"
 	"time"
 )
+
+// Backend contains backend specific fields
+type Backend struct {
+	PackageName                     string             `json:"package_name"`
+	PackageRevision                 string             `json:"package_revision"`
+	SystemUser                      string             `json:"system_user"`
+	Version                         string             `json:"version"`
+	BinaryURL                       string             `json:"binary_url"`
+	VerificationType                string             `json:"verification_type"`
+	VerificationSource              string             `json:"verification_source"`
+	ExtractCommand                  string             `json:"extract_command"`
+	ExcludeFiles                    []string           `json:"exclude_files"`
+	ExecCommandTemplate             string             `json:"exec_command_template"`
+	LogrotateFilesTemplate          string             `json:"logrotate_files_template"`
+	PostinstScriptTemplate          string             `json:"postinst_script_template"`
+	ServiceType                     string             `json:"service_type"`
+	ServiceAdditionalParamsTemplate string             `json:"service_additional_params_template"`
+	ProtectMemory                   bool               `json:"protect_memory"`
+	Mainnet                         bool               `json:"mainnet"`
+	ServerConfigFile                string             `json:"server_config_file"`
+	ClientConfigFile                string             `json:"client_config_file"`
+	AdditionalParams                interface{}        `json:"additional_params,omitempty"`
+	Platforms                       map[string]Backend `json:"platforms,omitempty"`
+}
 
 // Config contains the structure of the config
 type Config struct {
@@ -33,27 +59,7 @@ type Config struct {
 		RPCTimeout                  int    `json:"rpc_timeout"`
 		MessageQueueBindingTemplate string `json:"message_queue_binding_template"`
 	} `json:"ipc"`
-	Backend struct {
-		PackageName                     string      `json:"package_name"`
-		PackageRevision                 string      `json:"package_revision"`
-		SystemUser                      string      `json:"system_user"`
-		Version                         string      `json:"version"`
-		BinaryURL                       string      `json:"binary_url"`
-		VerificationType                string      `json:"verification_type"`
-		VerificationSource              string      `json:"verification_source"`
-		ExtractCommand                  string      `json:"extract_command"`
-		ExcludeFiles                    []string    `json:"exclude_files"`
-		ExecCommandTemplate             string      `json:"exec_command_template"`
-		LogrotateFilesTemplate          string      `json:"logrotate_files_template"`
-		PostinstScriptTemplate          string      `json:"postinst_script_template"`
-		ServiceType                     string      `json:"service_type"`
-		ServiceAdditionalParamsTemplate string      `json:"service_additional_params_template"`
-		ProtectMemory                   bool        `json:"protect_memory"`
-		Mainnet                         bool        `json:"mainnet"`
-		ServerConfigFile                string      `json:"server_config_file"`
-		ClientConfigFile                string      `json:"client_config_file"`
-		AdditionalParams                interface{} `json:"additional_params,omitempty"`
-	} `json:"backend"`
+	Backend   Backend `json:"backend"`
 	Blockbook struct {
 		PackageName             string `json:"package_name"`
 		SystemUser              string `json:"system_user"`
@@ -87,6 +93,7 @@ type Config struct {
 		BackendDataPath      string `json:"backend_data_path"`
 		BlockbookInstallPath string `json:"blockbook_install_path"`
 		BlockbookDataPath    string `json:"blockbook_data_path"`
+		Architecture         string `json:"architecture"`
 	} `json:"-"`
 }
 
@@ -136,6 +143,16 @@ func (c *Config) ParseTemplate() *template.Template {
 	return t
 }
 
+func copyNonZeroBackendFields(toValue *Backend, fromValue *Backend) {
+	from := reflect.ValueOf(*fromValue)
+	to := reflect.ValueOf(toValue).Elem()
+	for i := 0; i < from.NumField(); i++ {
+		if from.Field(i).IsValid() && !from.Field(i).IsZero() {
+			to.Field(i).Set(from.Field(i))
+		}
+	}
+}
+
 // LoadConfig loads the config files
 func LoadConfig(configsDir, coin string) (*Config, error) {
 	config := new(Config)
@@ -161,8 +178,15 @@ func LoadConfig(configsDir, coin string) (*Config, error) {
 	}
 
 	config.Meta.BuildDatetime = time.Now().Format("Mon, 02 Jan 2006 15:04:05 -0700")
+	config.Env.Architecture = runtime.GOARCH
 
 	if !isEmpty(config, "backend") {
+		// set platform specific fields to config
+		platform, found := config.Backend.Platforms[runtime.GOARCH]
+		if found {
+			copyNonZeroBackendFields(&config.Backend, &platform)
+		}
+
 		switch config.Backend.ServiceType {
 		case "forking":
 		case "simple":
