96a0ff857783d8e58f69c35c5a08a167476d023a
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2022-12-09 12:13:15 -0700

    add subscriber interfaces for subscription channels

--- bchain/coins/eth/rpc.go
+++ bchain/coins/eth/rpc.go
@@ -3,6 +3,8 @@ package eth
 import (
 	"context"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
@@ -41,10 +43,65 @@ type ClientSubscription interface {
 	Unsubscribe()
 }
 
+// EthereumClientSubscription wraps an ethereum client subcription to conform with the ClientSubscription interface
 type EthereumClientSubscription struct {
 	*rpc.ClientSubscription
 }
 
 func (c *EthereumClientSubscription) Unsubscribe() {
 	c.ClientSubscription.Unsubscribe()
+}
+
+// Subscriber interface for evm chains
+type Subscriber interface {
+	Channel() interface{}
+	Close()
+}
+
+// NewBlockSubscriber interface for evm chains
+type NewBlockSubscriber interface {
+	Subscriber
+	Read() (Header, bool)
+}
+
+// EthereumNewBlock wraps an ethereum header channel to conform with the Subscriber interface
+type EthereumNewBlock struct {
+	channel chan *types.Header
+}
+
+func (s *EthereumNewBlock) Channel() interface{} {
+	return s.channel
+}
+
+func (s *EthereumNewBlock) Read() (Header, bool) {
+	h, ok := <-s.channel
+	return &EthereumHeader{Header: h}, ok
+}
+
+func (s *EthereumNewBlock) Close() {
+	close(s.channel)
+}
+
+// NewTxSubscriber interface for evm chains
+type NewTxSubscriber interface {
+	Subscriber
+	Read() (Hash, bool)
+}
+
+// EthereumNewTx wraps an ethereum transaction hash channel to conform with the Subscriber interface
+type EthereumNewTx struct {
+	channel chan common.Hash
+}
+
+func (s *EthereumNewTx) Channel() interface{} {
+	return s.channel
+}
+
+func (s *EthereumNewTx) Read() (Hash, bool) {
+	h, ok := <-s.channel
+	return &EthereumHash{Hash: h}, ok
+}
+
+func (s *EthereumNewTx) Close() {
+	close(s.channel)
 }
