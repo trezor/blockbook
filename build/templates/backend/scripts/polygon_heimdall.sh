47db4dc71f418193d026e3f6bf8fb17330075130
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-02-16 17:38:59 -0700

    backend fixes

--- build/templates/backend/scripts/polygon_heimdall.sh
+++ build/templates/backend/scripts/polygon_heimdall.sh
@@ -7,24 +7,27 @@ set -e
 INSTALL_DIR={{.Env.BackendInstallPath}}/{{.Coin.Alias}}
 DATA_DIR={{.Env.BackendDataPath}}/{{.Coin.Alias}}/backend
 
+HEIMDALL_BIN=$INSTALL_DIR/heimdalld
 HOME_DIR=$DATA_DIR/heimdalld
 CONFIG_DIR=$HOME_DIR/config
 
 if [[ ! -d "$CONFIG_DIR" ]]; then
   # init chain
-  heimdalld init --home $HOME_DIR
+  $HEIMDALL_BIN init --home $HOME_DIR
 
   # overwrite genesis file
   cp $INSTALL_DIR/genesis.json $CONFIG_DIR/genesis.json
 fi
 
-heimdalld start \
+# bor_rpc_url: backend-polygon-bor service
+# eth_rpc_url: backend-ethereum service
+$HEIMDALL_BIN start \
   --home $HOME_DIR \
   --rpc.laddr tcp://127.0.0.1:{{.Ports.BackendRPC}} \
   --p2p.laddr tcp://0.0.0.0:{{.Ports.BackendP2P}} \
   --laddr tcp://127.0.0.1:{{.Ports.BackendHttp}} \
-  --bor_rpc_url http://127.0.0.1:8303 \ # backend-polygon-bor service
-  --eth_rpc_url http://127.0.0.1:8136 \ # backend-ethereum service
+  --bor_rpc_url http://127.0.0.1:8303 \
+  --eth_rpc_url http://127.0.0.1:8136 \
   --rest-server 
 
 {{end}}
