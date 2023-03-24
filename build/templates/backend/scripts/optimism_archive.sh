ae8a9afda9ee5025542bc9001a462cd47fae1213
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-03-24 13:10:43 -0600

    add mainnet configs and exec scripts (updates required on mainnet upgrade)

--- build/templates/backend/scripts/optimism_archive.sh
+++ build/templates/backend/scripts/optimism_archive.sh
@@ -8,7 +8,7 @@ GETH_BIN={{.Env.BackendInstallPath}}/{{.Coin.Alias}}/geth
 DATA_DIR={{.Env.BackendDataPath}}/{{.Coin.Alias}}/backend
 
 CHAINDATA_DIR=$DATA_DIR/geth/chaindata
-SNAPSHOT=https://storage.googleapis.com/oplabs-goerli-data/goerli-bedrock.tar
+SNAPSHOT=
 
 if [[ -n "$SNAPSHOT" && ! -d "$CHAINDATA_DIR" ]]; then
   wget -c $SNAPSHOT -O - | tar -xvf - -C $DATA_DIR
@@ -16,7 +16,7 @@ fi
 
 $GETH_BIN \
   --datadir=$DATA_DIR \
-  --networkid=420 \
+  --networkid=10 \
   --authrpc.addr=127.0.0.1 \
   --authrpc.port={{.Ports.BackendAuthRpc}} \
   --authrpc.vhosts="*" \
@@ -33,8 +33,8 @@ $GETH_BIN \
   --ws.api=eth,net,web3,debug,txpool,engine \
   --ws.origins="*" \
   --rollup.disabletxpoolgossip=true \
-  --rollup.historicalrpc=http://127.0.0.1:18303 \
-  --rollup.sequencerhttp=https://goerli-sequencer.optimism.io \
+  --rollup.historicalrpc=http://127.0.0.1:8303 \
+  --rollup.sequencerhttp=https://sequencer.optimism.io \
   --cache=4096 \
   --cache.gc=0 \
   --cache.trie=30 \
