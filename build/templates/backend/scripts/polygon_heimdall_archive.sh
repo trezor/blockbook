842d831d10f28c77958a67ed91ac24f853d8f89f
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-05-12 16:11:12 -0600

    add static peers

--- build/templates/backend/scripts/polygon_heimdall_archive.sh
+++ build/templates/backend/scripts/polygon_heimdall_archive.sh
@@ -26,6 +26,7 @@ $HEIMDALL_BIN start \
   --rpc.laddr tcp://127.0.0.1:{{.Ports.BackendRPC}} \
   --p2p.laddr tcp://0.0.0.0:{{.Ports.BackendP2P}} \
   --laddr tcp://127.0.0.1:{{.Ports.BackendHttp}} \
+  --p2p.seeds "2a53a15ffc70ad41b6876ecbe05c50a66af01e20@3.211.248.31:26656,6f829065789e5b156cbbf076f9d133b4d7725847@3.212.183.151:26656,7285a532bad665f051c0aadc31054e2e61ca2b3d@3.93.224.197:26656,0b431127d21c8970f1c353ab212be4f1ba86c3bf@184.73.124.158:26656,f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,31b79cf4a628a4619e8e9ae95b72e4354c5a5d90@44.232.55.71:26656,a385dd467d11c4cdb0be8b51d7bfb0990f49abc3@35.199.4.13:26656,daad548c0a163faae1d8d58425f97207acf923fd@35.230.116.151:26656,81c76e82fcc3dc9a0a1554a3edaa09a632795ea8@35.221.13.28:26656" \
   --node tcp://127.0.0.1:{{.Ports.BackendRPC}} \
   --bor_rpc_url http://127.0.0.1:8172 \
   --eth_rpc_url http://127.0.0.1:8116 \
