4b0b517a94ef6c7ebd95ef4b09de5d6c24a1d622
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-06-07 10:25:22 -0600

    update config and exec scripts

--- build/templates/backend/scripts/optimism.sh
+++ build/templates/backend/scripts/optimism.sh
@@ -8,36 +8,38 @@ GETH_BIN={{.Env.BackendInstallPath}}/{{.Coin.Alias}}/geth
 DATA_DIR={{.Env.BackendDataPath}}/{{.Coin.Alias}}/backend
 
 CHAINDATA_DIR=$DATA_DIR/geth/chaindata
-SNAPSHOT=
+SNAPSHOT=https://storage.googleapis.com/oplabs-mainnet-data/mainnet-bedrock.tar
 
-if [[ -n "$SNAPSHOT" && ! -d "$CHAINDATA_DIR" ]]; then
+if [ ! -d "$CHAINDATA_DIR" ]; then
   wget -c $SNAPSHOT -O - | tar -xvf - -C $DATA_DIR
 fi
 
 $GETH_BIN \
-  --datadir=$DATA_DIR \
-  --networkid=10 \
-  --authrpc.addr=127.0.0.1 \
-  --authrpc.port={{.Ports.BackendAuthRpc}} \
-  --authrpc.vhosts="*" \
-  --port={{.Ports.BackendP2P}} \
+  --networkid 10 \
+  --datadir $DATA_DIR \
+  --authrpc.jwtsecret $DATA_DIR/jwtsecret \
+  --authrpc.addr 127.0.0.1 \
+  --authrpc.port {{.Ports.BackendAuthRpc}} \
+  --authrpc.vhosts "*" \
+  --port {{.Ports.BackendP2P}} \
   --http \
-  --http.port={{.Ports.BackendHttp}} \
-  --http.addr=127.0.0.1 \
-  --http.api=eth,net,web3,debug,txpool,engine \
-  --http.vhosts="*" \
-  --http.corsdomain="*" \
+  --http.port {{.Ports.BackendHttp}} \
+  --http.addr 127.0.0.1 \
+  --http.api eth,net,web3,debug,txpool,engine \
+  --http.vhosts "*" \
+  --http.corsdomain "*" \
   --ws \
-  --ws.port={{.Ports.BackendRPC}} \
-  --ws.addr=127.0.0.1 \
-  --ws.api=eth,net,web3,debug,txpool,engine \
-  --ws.origins="*" \
+  --ws.port {{.Ports.BackendRPC}} \
+  --ws.addr 127.0.0.1 \
+  --ws.api eth,net,web3,debug,txpool,engine \
+  --ws.origins "*" \
   --rollup.disabletxpoolgossip=true \
-  --rollup.sequencerhttp=https://sequencer.optimism.io \
-  --cache=4096 \
-  --syncmode=full \
-  --gcmode=full \
-  --maxpeers=0 \
+  --rollup.sequencerhttp https://mainnet-sequencer.optimism.io \
+  --txlookuplimit 0 \
+  --cache 4096 \
+  --syncmode full \
+  --gcmode full \
+  --maxpeers 0 \
   --nodiscover
 
 {{end}}
