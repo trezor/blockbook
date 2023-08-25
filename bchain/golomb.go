c1c4a9c62fae1e0866511464e41c76eb7455f8e1
grdddj <jiri.musil06@seznam.cz>
2023-08-25 15:07:34 +0000

    WIP - ignoring ordinals parent transactions

--- bchain/golomb.go
+++ bchain/golomb.go
@@ -27,6 +27,10 @@ type GolombFilter struct {
 	filterScriptsType FilterScriptsType
 	filterData        [][]byte
 	uniqueData        map[string]struct{}
+	// All the unique txids that contain ordinal data
+	ordinalTxIds map[string]struct{}
+	// Mapping of txid to address descriptors - only used in case of taproot-noordinals
+	allAddressDescriptors map[string][]AddressDescriptor
 }
 
 // NewGolombFilter initializes the GolombFilter handler
@@ -43,9 +47,14 @@ func NewGolombFilter(p uint8, filterScripts string, key string) (*GolombFilter, 
 		filterData:        make([][]byte, 0),
 		uniqueData:        make(map[string]struct{}),
 	}
-	// only taproot and all is supported
+	// reject invalid filterScripts
 	if gf.filterScriptsType == FilterScriptsInvalid {
 		return nil, errors.Errorf("Invalid/unsupported filterScripts parameter %s", filterScripts)
+	}
+	// set ordinal-related fields if needed
+	if gf.ignoreOrdinals() {
+		gf.ordinalTxIds = make(map[string]struct{})
+		gf.allAddressDescriptors = make(map[string][]AddressDescriptor)
 	}
 	return &gf, nil
 }
@@ -65,6 +74,7 @@ func isInputOrdinal(vin Vin) bool {
 	return len(vin.Witness) > 2 && bytes.Contains(vin.Witness[1], byte_pattern)
 }
 
+// Whether a transaction contains any ordinal data
 func txContainsOrdinal(tx *Tx) bool {
 	for _, vin := range tx.Vin {
 		if isInputOrdinal(vin) {
@@ -74,17 +84,42 @@ func txContainsOrdinal(tx *Tx) bool {
 	return false
 }
 
+// Saving all the ordinal-related txIds so we can later ignore their address descriptors
+func (f *GolombFilter) markTxAndParentsAsOrdinals(tx *Tx) {
+	f.ordinalTxIds[tx.Txid] = struct{}{}
+	for _, vin := range tx.Vin {
+		f.ordinalTxIds[vin.Txid] = struct{}{}
+	}
+}
+
+// Adding a new address descriptor mapped to a txid
+func (f *GolombFilter) addTxIdMapping(ad AddressDescriptor, tx *Tx) {
+	f.allAddressDescriptors[tx.Txid] = append(f.allAddressDescriptors[tx.Txid], ad)
+}
+
 // AddAddrDesc adds taproot address descriptor to the data for the filter
 func (f *GolombFilter) AddAddrDesc(ad AddressDescriptor, tx *Tx) {
 	if f.ignoreNonTaproot() && !ad.IsTaproot() {
 		return
 	}
 	if f.ignoreOrdinals() && tx != nil && txContainsOrdinal(tx) {
+		f.markTxAndParentsAsOrdinals(tx)
 		return
 	}
 	if len(ad) == 0 {
 		return
 	}
+	// When ignoring ordinals, we need to save all the address descriptors before
+	// filtering out the "invalid" ones.
+	if f.ignoreOrdinals() && tx != nil {
+		f.addTxIdMapping(ad, tx)
+		return
+	}
+	f.includeAddrDesc(ad)
+}
+
+// Private function to be called with descriptors that were already validated
+func (f *GolombFilter) includeAddrDesc(ad AddressDescriptor) {
 	s := string(ad)
 	if _, found := f.uniqueData[s]; !found {
 		f.filterData = append(f.filterData, ad)
@@ -92,9 +127,27 @@ func (f *GolombFilter) AddAddrDesc(ad AddressDescriptor, tx *Tx) {
 	}
 }
 
+// Including all the address descriptors from non-ordinal transactions
+func (f *GolombFilter) includeAllAddressDescriptorsOrdinals() {
+	for txid, ads := range f.allAddressDescriptors {
+		// Ignoring the txids that contain ordinal data
+		if _, found := f.ordinalTxIds[txid]; found {
+			continue
+		}
+		for _, ad := range ads {
+			f.includeAddrDesc(ad)
+		}
+	}
+}
+
 // Compute computes golomb filter from the data
 func (f *GolombFilter) Compute() []byte {
 	m := uint64(1 << uint64(f.p))
+
+	// In case of ignoring the ordinals, we still need to assemble the filter data
+	if f.ignoreOrdinals() {
+		f.includeAllAddressDescriptorsOrdinals()
+	}
 
 	if len(f.filterData) == 0 {
 		return nil
