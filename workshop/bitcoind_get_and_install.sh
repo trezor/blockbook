a5426025f89968dc9e687660343928c9b81e6e31
grdddj <jiri.musil06@seznam.cz>
2024-11-25 14:32:04 +0100

    chore: update the bitcoind version to 28.1

--- workshop/bitcoind_get_and_install.sh
+++ workshop/bitcoind_get_and_install.sh
@@ -5,20 +5,20 @@ set -e
 echo "Going to /tmp..."
 cd /tmp
 
-TARBALL=bitcoin-27.1-x86_64-linux-gnu.tar.gz
+TARBALL=bitcoin-28.0-x86_64-linux-gnu.tar.gz
 
 echo "Removing previously downloaded tarball when it exists..."
 rm $TARBALL || true
 
-echo "Downloading bitcoincore 27.1 tarball..."
-wget https://bitcoincore.org/bin/bitcoin-core-27.1/$TARBALL
+echo "Downloading bitcoincore 28.0 tarball..."
+wget https://bitcoincore.org/bin/bitcoin-core-28.0/$TARBALL
 
 
 echo "Extracing the tarball..."
 tar xzf $TARBALL
 
 echo "Navigating to the directory with bitcoind and bitcoin-cli binaries..."
-cd bitcoin-27.1
+cd bitcoin-28.0
 cd bin
 
 echo "Installing bitcoind - please supply sudo password"
