964662d578825e90e9608ace0f4ac91771c2d6b1
Martin Boehm <martin.boehm@1mbsoftware.net>
2025-11-24 02:24:39 +0100

    Upgrade golang to 1.25 and dependencies, fix Avalanche sync

--- bchain/coins/avalanche/types.go
+++ bchain/coins/avalanche/types.go
@@ -3,33 +3,16 @@ package avalanche
 import (
 	"encoding/json"
 	"errors"
-	"io"
 	"math/big"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rlp"
-	"golang.org/x/crypto/sha3"
 )
-
-var hasherPool = sync.Pool{
-	New: func() interface{} { return sha3.NewLegacyKeccak256() },
-}
-
-func rlpHash(x interface{}) (h common.Hash) {
-	sha := hasherPool.Get().(crypto.KeccakState)
-	defer hasherPool.Put(sha)
-	sha.Reset()
-	_ = rlp.Encode(sha, x)
-	_, _ = sha.Read(h[:])
-	return h
-}
 
 // Header represents a block header in the Avalanche blockchain.
 type Header struct {
+	RpcHash     common.Hash      `json:"hash"             gencodec:"required"`
 	ParentHash  common.Hash      `json:"parentHash"       gencodec:"required"`
 	UncleHash   common.Hash      `json:"sha3Uncles"       gencodec:"required"`
 	Coinbase    common.Address   `json:"miner"            gencodec:"required"`
@@ -128,6 +111,7 @@ func (h Header) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON unmarshals from JSON.
 func (h *Header) UnmarshalJSON(input []byte) error {
 	type Header struct {
+		RpcHash          *common.Hash      `json:"hash"`
 		ParentHash       *common.Hash      `json:"parentHash"       gencodec:"required"`
 		UncleHash        *common.Hash      `json:"sha3Uncles"       gencodec:"required"`
 		Coinbase         *common.Address   `json:"miner"            gencodec:"required"`
@@ -155,6 +139,10 @@ func (h *Header) UnmarshalJSON(input []byte) error {
 	if err := json.Unmarshal(input, &dec); err != nil {
 		return err
 	}
+	if dec.RpcHash == nil {
+		return errors.New("missing required field 'hash' for Header")
+	}
+	h.RpcHash = *dec.RpcHash
 	if dec.ParentHash == nil {
 		return errors.New("missing required field 'parentHash' for Header")
 	}
@@ -238,102 +226,7 @@ func (h *Header) UnmarshalJSON(input []byte) error {
 	return nil
 }
 
-func (obj *Header) EncodeRLP(_w io.Writer) error {
-	w := rlp.NewEncoderBuffer(_w)
-	_tmp0 := w.List()
-	w.WriteBytes(obj.ParentHash[:])
-	w.WriteBytes(obj.UncleHash[:])
-	w.WriteBytes(obj.Coinbase[:])
-	w.WriteBytes(obj.Root[:])
-	w.WriteBytes(obj.TxHash[:])
-	w.WriteBytes(obj.ReceiptHash[:])
-	w.WriteBytes(obj.Bloom[:])
-	if obj.Difficulty == nil {
-		_, _ = w.Write(rlp.EmptyString)
-	} else {
-		if obj.Difficulty.Sign() == -1 {
-			return rlp.ErrNegativeBigInt
-		}
-		w.WriteBigInt(obj.Difficulty)
-	}
-	if obj.Number == nil {
-		_, _ = w.Write(rlp.EmptyString)
-	} else {
-		if obj.Number.Sign() == -1 {
-			return rlp.ErrNegativeBigInt
-		}
-		w.WriteBigInt(obj.Number)
-	}
-	w.WriteUint64(obj.GasLimit)
-	w.WriteUint64(obj.GasUsed)
-	w.WriteUint64(obj.Time)
-	w.WriteBytes(obj.Extra)
-	w.WriteBytes(obj.MixDigest[:])
-	w.WriteBytes(obj.Nonce[:])
-	w.WriteBytes(obj.ExtDataHash[:])
-	_tmp1 := obj.BaseFee != nil
-	_tmp2 := obj.ExtDataGasUsed != nil
-	_tmp3 := obj.BlockGasCost != nil
-	_tmp4 := obj.BlobGasUsed != nil
-	_tmp5 := obj.ExcessBlobGas != nil
-	_tmp6 := obj.ParentBeaconRoot != nil
-	if _tmp1 || _tmp2 || _tmp3 || _tmp4 || _tmp5 || _tmp6 {
-		if obj.BaseFee == nil {
-			_, _ = w.Write(rlp.EmptyString)
-		} else {
-			if obj.BaseFee.Sign() == -1 {
-				return rlp.ErrNegativeBigInt
-			}
-			w.WriteBigInt(obj.BaseFee)
-		}
-	}
-	if _tmp2 || _tmp3 || _tmp4 || _tmp5 || _tmp6 {
-		if obj.ExtDataGasUsed == nil {
-			_, _ = w.Write(rlp.EmptyString)
-		} else {
-			if obj.ExtDataGasUsed.Sign() == -1 {
-				return rlp.ErrNegativeBigInt
-			}
-			w.WriteBigInt(obj.ExtDataGasUsed)
-		}
-	}
-	if _tmp3 || _tmp4 || _tmp5 || _tmp6 {
-		if obj.BlockGasCost == nil {
-			_, _ = w.Write(rlp.EmptyString)
-		} else {
-			if obj.BlockGasCost.Sign() == -1 {
-				return rlp.ErrNegativeBigInt
-			}
-			w.WriteBigInt(obj.BlockGasCost)
-		}
-	}
-	if _tmp4 || _tmp5 || _tmp6 {
-		if obj.BlobGasUsed == nil {
-			_, _ = w.Write([]byte{0x80})
-		} else {
-			w.WriteUint64((*obj.BlobGasUsed))
-		}
-	}
-	if _tmp5 || _tmp6 {
-		if obj.ExcessBlobGas == nil {
-			_, _ = w.Write([]byte{0x80})
-		} else {
-			w.WriteUint64((*obj.ExcessBlobGas))
-		}
-	}
-	if _tmp6 {
-		if obj.ParentBeaconRoot == nil {
-			_, _ = w.Write([]byte{0x80})
-		} else {
-			w.WriteBytes(obj.ParentBeaconRoot[:])
-		}
-	}
-	w.ListEnd(_tmp0)
-	return w.Flush()
-}
-
-// Hash returns the block hash of the header, which is simply the keccak256 hash of its
-// RLP encoding.
+// Hash returns the block hash of the header
 func (h *Header) Hash() common.Hash {
-	return rlpHash(h)
+	return h.RpcHash
 }
