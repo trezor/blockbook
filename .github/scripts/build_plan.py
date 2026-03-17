7d37154c6485acfccd931f3112c248606e398bc9
pragmaxim <pragmaxim@gmail.com>
2026-03-17 20:30:51 +0100

    ci/cd: cleanup

--- .github/scripts/build_plan.py
+++ .github/scripts/build_plan.py
@@ -4,7 +4,7 @@ import json
 import os
 from pathlib import Path
 
-from plan_common import fail, load_runner_map, parse_requested_coins
+from runner import fail, load_runner_map, parse_requested_coins
 
 
 def main() -> None:
