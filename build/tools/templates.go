a6f9a4d5682d479ea6a526ada360c589dc4152be
jhawk28 <jhawk28@gmail.com>
2026-09-14 07:49:59 -0400

    Add CurveZMQ support for encrypted message queue binding (#1784)
    Optional ipc.message_queue_curve in coin definitions is copied into
    generated blockchaincfg.json (public_key, secret_key, server_key). Empty
    client keys generate a keypair at connect time. Stock Bitcoin Core does
    not publish CurveZMQ; this is for a proxy or a patched backend.

--- build/tools/templates.go
+++ build/tools/templates.go
@@ -44,6 +44,14 @@ type Backend struct {
 	Platforms                       map[string]Backend `json:"platforms,omitempty"`
 }
 
+// MessageQueueCurve is optional ZMQ CURVE key material from a coin definition
+// (`ipc.message_queue_curve`). Copied into blockchaincfg.json as message_queue_curve.
+type MessageQueueCurve struct {
+	PublicKey string `json:"public_key,omitempty"`
+	SecretKey string `json:"secret_key,omitempty"`
+	ServerKey string `json:"server_key,omitempty"`
+}
+
 // Config contains the structure of the config
 type Config struct {
 	Coin struct {
@@ -64,12 +72,13 @@ type Config struct {
 		BlockbookPublic     int `json:"blockbook_public"`
 	} `json:"ports"`
 	IPC struct {
-		RPCURLTemplate              string `json:"rpc_url_template"`
-		RPCURLWSTemplate            string `json:"rpc_url_ws_template"`
-		RPCUser                     string `json:"rpc_user"`
-		RPCPass                     string `json:"rpc_pass"`
-		RPCTimeout                  int    `json:"rpc_timeout"`
-		MessageQueueBindingTemplate string `json:"message_queue_binding_template"`
+		RPCURLTemplate              string             `json:"rpc_url_template"`
+		RPCURLWSTemplate            string             `json:"rpc_url_ws_template"`
+		RPCUser                     string             `json:"rpc_user"`
+		RPCPass                     string             `json:"rpc_pass"`
+		RPCTimeout                  int                `json:"rpc_timeout"`
+		MessageQueueBindingTemplate string             `json:"message_queue_binding_template"`
+		MessageQueueCurve           *MessageQueueCurve `json:"message_queue_curve,omitempty"`
 	} `json:"ipc"`
 	Backend   Backend `json:"backend"`
 	Blockbook struct {
@@ -127,8 +136,8 @@ const (
 	devBlockbookPprofPortOffset = 20000
 )
 
-func jsonToString(msg json.RawMessage) (string, error) {
-	d, err := msg.MarshalJSON()
+func jsonToString(msg any) (string, error) {
+	d, err := json.Marshal(msg)
 	if err != nil {
 		return "", err
 	}
